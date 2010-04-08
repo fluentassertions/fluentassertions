a5a98d0e4cde428c9eeb396828846f10b6375ee3
dennisdoomen <none@none.com>
2010-04-08 18:14:59 +0000

    Added a missing file.

---
+++ Main/FluentAssertions/ActionAssertions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentAssertions
+{
+    public class ActionAssertions<TException> : Assertions<Action, ActionAssertions<TException>> where TException : Exception
+    {
+        private Exception exception;
+
+        public ActionAssertions(Action action, string reason, object[] reasonParameters)
+        {
+            exception = null;
+
+            try
+            {
+                action();
+            }
+            catch (Exception actualException)
+            {
+                exception = actualException;
+            }
+
+            VerifyThat(exception != null, "Expected {0}{2}, but no exception was thrown.",
+                typeof(TException), null, reason, reasonParameters);
+
+            VerifyThat(exception is TException,
+                "Expected {0}{2}, but found {1}.",
+                typeof(TException), exception.GetType(), reason, reasonParameters);
+
+        }
+
+        public ExceptionAssertions<TException> ExceptionAssertions
+        {
+            get
+            {
+                return new ExceptionAssertions<TException>((TException) exception);            
+            }
+        }
+    }
+}
