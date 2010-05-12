d039b319ade0965ad6e9f242dee3f3df3a33430f
dennisdoomen <none@none.com>
2010-05-12 09:23:55 +0000

    Added specific versions for XUnit, NUnit, MSTest in VS2009 and VS2010. Changed the version to 1.2.1

---
+++ Main/FluentAssertions/Frameworks/SpecificationMismatchException.MSTest.cs
@@ -0,0 +1,16 @@
+#if MSTEST
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentAssertions
+{
+    public class SpecificationMismatchException : UnitTestAssertException
+    {
+        public SpecificationMismatchException(string message) : base(message)
+        {
+            
+        }
+    }
+}
+
+#endif
