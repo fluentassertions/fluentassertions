1ea1516fde99ddf8ae37744ba9ca1797ec0f4ecb
dennisdoomen <none@none.com>
2011-01-13 18:42:39 +0000

    Refactored the generic WeakReference to use composition instead of inheritance.

--- Main/FluentAssertions.Net35/Common/WeakKeyReference.cs
+++ Main/FluentAssertions.Net35/Common/WeakKeyReference.cs
@@ -4,8 +4,7 @@
     {
         public readonly int HashCode;
 
-        public WeakKeyReference(T key, WeakKeyComparer<T> comparer)
-            : base(key)
+        public WeakKeyReference(T key, WeakKeyComparer<T> comparer) : base(key)
         {
             // retain the object's hash code immediately so that even
             // if the target is GC'ed we will be able to find and
