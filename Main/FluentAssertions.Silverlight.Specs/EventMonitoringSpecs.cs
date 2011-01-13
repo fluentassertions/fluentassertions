b4a00525b25ecb5046aa8059de16e0e0066641fc
dennisdoomen <none@none.com>
2011-01-13 18:38:50 +0000

    Added a Silverlight unit test project.

---
+++ Main/FluentAssertions.Silverlight.Specs/EventMonitoringSpecs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions.EventMonitoring;
+
+namespace FluentAssertions.Silverlight.Specs
+{
+    [TestClass]
+    public class EventMonitoringSpecs
+    {
+        [TestMethod]
+        public void When_WHEN_it_should_SHOULD()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //----------------------------------------------------------------------------------------------------------
+            var subject = new TestViewModel();
+            subject.MonitorEvents();
+            subject.ChangeProperty("value");
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            Action act = () => subject.ShouldRaisePropertyChangeFor(v => v.SomeProperty);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            act.ShouldNotThrow(); 
+        }
+    }
+}
