eb1862c8bf4cf02a9b2b4ce7ddcd0132d448e326
dennisdoomen <none@none.com>
2011-01-14 18:56:52 +0000

    Limited event monitoring in Silverlight to INotifyPropertyChanged because of ILGen limitations in SL4.

--- Main/FluentAssertions.Silverlight.Specs/EventMonitoringSpecs.cs
+++ Main/FluentAssertions.Silverlight.Specs/EventMonitoringSpecs.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Silverlight.Testing;
+using System.ComponentModel;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using FluentAssertions.EventMonitoring;
@@ -19,24 +11,91 @@ namespace FluentAssertions.Silverlight.Specs
     public class EventMonitoringSpecs
     {
         [TestMethod]
-        public void When_WHEN_it_should_SHOULD()
+        public void When_a_property_changed_event_for_a_specific_property_was_not_raised_it_should_throw()
         {
             //-----------------------------------------------------------------------------------------------------------
             // Arrange
             //----------------------------------------------------------------------------------------------------------
-            var subject = new TestViewModel();
+            var subject = new EventRaisingClass();
             subject.MonitorEvents();
-            subject.ChangeProperty("value");
 
             //-----------------------------------------------------------------------------------------------------------
             // Act
             //-----------------------------------------------------------------------------------------------------------
-            Action act = () => subject.ShouldRaisePropertyChangeFor(v => v.SomeProperty);
+            Action act = () => subject.ShouldRaisePropertyChangeFor(x => x.SomeProperty, "the property was changed");
 
             //-----------------------------------------------------------------------------------------------------------
             // Assert
             //-----------------------------------------------------------------------------------------------------------
-            act.ShouldNotThrow(); 
+            act.ShouldThrow<AssertFailedException>().WithMessage(
+                "Expected object <" + subject + "> to raise event \"PropertyChanged\" because the property was changed, but it did not.");
+        }
+        
+        [TestMethod]
+        public void When_a_property_changed_event_for_a_specific_property_was_raised_but_without_sender_it_should_throw()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //----------------------------------------------------------------------------------------------------------
+            var subject = new EventRaisingClass();
+            subject.MonitorEvents();
+            subject.RaiseEventWithoutSender();
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            Action act = () => subject
+                .ShouldRaisePropertyChangeFor(x => x.SomeProperty, "the property was changed")
+                .WithSender(subject);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            act.ShouldThrow<AssertFailedException>().WithMessage("Expected sender <" + subject + ">, but found <null>.");
+        }
+
+        [TestMethod]
+        public void When_a_property_changed_event_was_raised_for_the_right_property_it_should_not_throw()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //----------------------------------------------------------------------------------------------------------
+            var subject = new EventRaisingClass();
+            subject.MonitorEvents();
+            subject.RaiseEventWithSenderAndPropertyName("SomeProperty");
+            subject.RaiseEventWithSenderAndPropertyName("SomeOtherProperty");
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            Action act = () => subject.ShouldRaisePropertyChangeFor(x => x.SomeProperty);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            act.ShouldNotThrow();
+        }
+
+        internal class EventRaisingClass : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+            public void RaiseEventWithoutSender()
+            {
+                PropertyChanged(null, new PropertyChangedEventArgs("SomeProperty"));
+            }
+
+            public void RaiseEventWithSender()
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("SomeProperty"));
+            }
+
+            public void RaiseEventWithSenderAndPropertyName(string propertyName)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            public string SomeProperty { get; set; }
         }
     }
 }
---
+++ Main/FluentAssertions.Silverlight/EventMonitoring/EventMonitoringExtensions.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+
+using FluentAssertions.Common;
+
+namespace FluentAssertions.EventMonitoring
+{
+    /// <summary>
+    ///   Provides extension methods for monitoring and querying events.
+    /// </summary>
+    [DebuggerNonUserCode]
+    public static class EventMonitoringExtensions
+    {
+        private static readonly WeakDictionary<object, EventRecorder[]> eventRecordersMap =
+            new WeakDictionary<object, EventRecorder[]>();
+
+        /// <summary>
+        ///   Starts monitoring an object for its <see cref="INotifyPropertyChanged.PropertyChanged"/> events.
+        /// </summary>
+        /// <exception cref = "ArgumentNullException">Thrown if eventSource is Null.</exception>
+        public static IEnumerable<EventRecorder> MonitorEvents(this INotifyPropertyChanged eventSource)
+        {
+            if (eventSource == null)
+            {
+                throw new NullReferenceException("Cannot monitor the events of a <null> object.");
+            }
+
+            var recorders = new[] {CreateEventHandler(eventSource)};
+
+            if (eventRecordersMap.ContainsKey(eventSource))
+            {
+                eventRecordersMap.Remove(eventSource);
+            }
+
+            eventRecordersMap.Add(eventSource, recorders);
+
+            return recorders;
+        }
+
+        private static EventRecorder CreateEventHandler(INotifyPropertyChanged eventSource)
+        {
+            var eventRecorder = new EventRecorder(eventSource, "PropertyChanged");
+
+            eventSource.PropertyChanged += (sender, args) => eventRecorder.RecordEvent(sender, args);
+            
+            return eventRecorder;
+        }
+
+        /// <summary>
+        /// Asserts that an object has raised a particular event at least once.
+        /// </summary>
+        /// <param name="eventName">
+        /// The name of the event that should have been raised.
+        /// </param>
+        /// <param name="reason">
+        /// A formatted phrase explaining why the assertion should be satisfied. If the phrase does not 
+        /// start with the word <i>because</i>, it is prepended to the message.
+        /// </param>
+        /// <param name="reasonParameters">
+        /// Zero or more values to use for filling in any <see cref="string.Format(string,object[])"/> compatible placeholders.
+        /// </param>
+        /// <remarks>
+        /// You must call <see cref="MonitorEvents"/> on the same object prior to this call so that Fluent Assertions can
+        /// subscribe for the events of the object.
+        /// </remarks>
+        internal static EventRecorder ShouldRaise(
+            this object eventSource, string eventName, string reason, params object[] reasonParameters)
+        {
+            if (!eventRecordersMap.ContainsKey(eventSource))
+            {
+                throw new InvalidOperationException(
+                    string.Format(
+                        "Object <{0}> is not being monitored for events. Use the MonitorEvents() extension method to start monitoring events.",
+                        eventSource));
+            }
+
+            EventRecorder eventRecorder = eventRecordersMap[eventSource].FirstOrDefault(r => r.EventName == eventName);
+            if (eventRecorder == null)
+            {
+                throw new InvalidOperationException(string.Format(
+                    "Object <{0}> does not expose an event named \"{1}\".", eventSource, eventName));
+            }
+
+            if (!eventRecorder.Any())
+            {
+                Verification.Fail("Expected object {1} to raise event {0}{2}, but it did not.", eventName, eventSource,
+                    reason, reasonParameters);
+            }
+
+            return eventRecorder;
+        }
+
+        /// <summary>
+        /// Asserts that an object has raised the <see cref="INotifyPropertyChanged.PropertyChanged"/> event for a particular property.
+        /// </summary>
+        /// <remarks>
+        /// You must call <see cref="MonitorEvents"/> on the same object prior to this call so that Fluent Assertions can
+        /// subscribe for the events of the object.
+        /// </remarks>
+        public static IEventRecorder ShouldRaisePropertyChangeFor<T>(
+            this T eventSource, Expression<Func<T, object>> propertyExpression)
+        {
+            return ShouldRaisePropertyChangeFor(eventSource, propertyExpression, "");
+        }
+
+        /// <summary>
+        /// Asserts that an object has raised the <see cref="INotifyPropertyChanged.PropertyChanged"/> event for a particular property.
+        /// </summary>
+        /// <param name="reason">
+        /// A formatted phrase explaining why the assertion should be satisfied. If the phrase does not 
+        /// start with the word <i>because</i>, it is prepended to the message.
+        /// </param>
+        /// <param name="reasonParameters">
+        /// Zero or more values to use for filling in any <see cref="string.Format(string,object[])"/> compatible placeholders.
+        /// </param>
+        /// <remarks>
+        /// You must call <see cref="MonitorEvents"/> on the same object prior to this call so that Fluent Assertions can
+        /// subscribe for the events of the object.
+        /// </remarks>
+        public static IEventRecorder ShouldRaisePropertyChangeFor<T>(
+            this T eventSource, Expression<Func<T, object>> propertyExpression,
+            string reason, params object[] reasonParameters)
+        {
+            return 
+                ShouldRaise(eventSource, "PropertyChanged", reason, reasonParameters)
+                .WithArgs<PropertyChangedEventArgs>(args => args.PropertyName == propertyExpression.GetPropertyInfo().Name);
+        }
+
+        /// <summary>
+        /// Asserts that all occurences of the event originated from the <param name="expectedSender"/>.
+        /// </summary>
+        public static IEventRecorder WithSender(this IEventRecorder eventRecorder, object expectedSender)
+        {
+            foreach (RecordedEvent recordedEvent in eventRecorder)
+            {
+                if (recordedEvent.Parameters.Length == 0)
+                {
+                    throw new ArgumentException(string.Format(
+                        "Expected event from sender <{0}>, but event {1} does not include any arguments",
+                        expectedSender, eventRecorder.EventName));
+                }
+
+                object actualSender = recordedEvent.Parameters.First();
+                Verification.Verify(ReferenceEquals(actualSender, expectedSender),
+                    "Expected sender {0}, but found {1}.", expectedSender, actualSender, "");
+            }
+
+            return eventRecorder;
+        }
+
+        /// <summary>
+        /// Asserts that at least one occurrence of the event had an <see cref="EventArgs"/> object matching a predicate.
+        /// </summary>
+        internal static IEventRecorder WithArgs<T>(this IEventRecorder eventRecorder, Expression<Func<T, bool>> predicate) where T : EventArgs
+        {
+            Func<T, bool> compiledPredicate = predicate.Compile();
+
+            if (eventRecorder.First().Parameters.OfType<T>().Count() == 0)
+            {
+                throw new ArgumentException("No argument of event " + eventRecorder.EventName + " is of type <" + typeof(T) + ">.");
+            }
+
+            if (!eventRecorder.Any(@event => compiledPredicate(@event.Parameters.OfType<T>().Single())))
+            {
+                Verification.Fail(
+                    "Expected at least one event with arguments matching {0}, but found none.", 
+                    predicate.Body, null, "", null);
+            }
+
+            return eventRecorder;
+        }
+    }
+}
