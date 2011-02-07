b9993937c10a16cb65bb6cfc6c3436f303eff9fe
mopdam <none@none.com>
2011-02-07 20:15:26 +0000

    IntExtension added to allow assertions like: theDateTime.Should().BeLessThan(10.Minutes().Before(otherDateTime));

---
+++ Main/FluentAssertions.Net35/Common/IntExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FluentAssertions.Common
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Returns a <see cref = "TimeSpan" /> for the current number of minutes
+        /// </summary>
+        public static TimeSpan Minutes(this int source)
+        {
+            return TimeSpan.FromMinutes(source);
+        }
+
+        /// <summary>
+        /// Returns a <see cref = "TimeSpan" /> for the current number of hours
+        /// </summary>
+        public static TimeSpan Hours(this int source)
+        {
+            return TimeSpan.FromHours(source);
+        }
+
+        /// <summary>
+        /// Returns a <see cref = "TimeSpan" /> for the current number of days
+        /// </summary>
+        public static TimeSpan Days(this int source)
+        {
+            return TimeSpan.FromDays(source);
+        }
+    }
+}
