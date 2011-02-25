97e6ab5534237b7312fa22d17f6a89faced178c4
dennisdoomen <none@none.com>
2011-02-25 20:21:17 +0000

    Added dedicated methods for asserting floating point values against an approximate value.

--- Main/FluentAssertions.Net35/FloatingPointExtensions.cs
+++ Main/FluentAssertions.Net35/FloatingPointExtensions.cs
@@ -20,6 +20,58 @@ namespace FluentAssertions
         /// <param name="reasonArgs">
         /// Zero or more objects to format using the placeholders in <see cref="reason"/>.
         /// </param>
+        public static AndConstraint<NumericAssertions<float?>> BeApproximately(this NumericAssertions<float?> parent, float expectedValue, float precision)
+        {
+            return BeApproximately(parent, expectedValue, precision, "");
+        }
+
+        /// <summary>
+        /// Asserts a floating point value approximates another value as close as possible.
+        /// </summary>
+        /// <param name="expectedValue">
+        /// The expected value to compare the actual value with.
+        /// </param>
+        /// <param name="precision">
+        /// The maximum amount of which the two values may differ.
+        /// </param>
+        /// <param name="reason">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])"/> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="reasonArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="reason"/>.
+        /// </param>
+        public static AndConstraint<NumericAssertions<float?>> BeApproximately(this NumericAssertions<float?> parent, float expectedValue, float precision, string reason,
+            params object[] reasonArgs)
+        {
+            if (parent.Subject == null)
+            {
+                Verification.Fail(
+                    "Expected value <" + parent.Subject + "> to approximate <" + expectedValue + "> +/- {0}{2}, but it was <null>.",
+                    precision, null, reason, reasonArgs);
+            }
+
+            BeApproximately(parent, expectedValue, precision, reason, reasonArgs);
+
+            return new AndConstraint<NumericAssertions<float?>>(parent);
+        }
+
+        /// <summary>
+        /// Asserts a floating point value approximates another value as close as possible.
+        /// </summary>
+        /// <param name="expectedValue">
+        /// The expected value to compare the actual value with.
+        /// </param>
+        /// <param name="precision">
+        /// The maximum amount of which the two values may differ.
+        /// </param>
+        /// <param name="reason">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])"/> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="reasonArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="reason"/>.
+        /// </param>
         public static AndConstraint<NumericAssertions<float>> BeApproximately(this NumericAssertions<float> parent, float expectedValue, float precision)
         {
             return BeApproximately(parent, expectedValue, precision, "");
@@ -55,6 +107,58 @@ namespace FluentAssertions
             }
 
             return new AndConstraint<NumericAssertions<float>>(parent);
+        }
+
+        /// <summary>
+        /// Asserts a floating point value approximates another value as close as possible.
+        /// </summary>
+        /// <param name="expectedValue">
+        /// The expected value to compare the actual value with.
+        /// </param>
+        /// <param name="precision">
+        /// The maximum amount of which the two values may differ.
+        /// </param>
+        /// <param name="reason">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])"/> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="reasonArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="reason"/>.
+        /// </param>
+        public static AndConstraint<NumericAssertions<double?>> BeApproximately(this NumericAssertions<double?> parent, double expectedValue, double precision)
+        {
+            return BeApproximately(parent, expectedValue, precision, "");
+        }
+
+        /// <summary>
+        /// Asserts a floating point value approximates another value as close as possible.
+        /// </summary>
+        /// <param name="expectedValue">
+        /// The expected value to compare the actual value with.
+        /// </param>
+        /// <param name="precision">
+        /// The maximum amount of which the two values may differ.
+        /// </param>
+        /// <param name="reason">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])"/> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="reasonArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="reason"/>.
+        /// </param>
+        public static AndConstraint<NumericAssertions<double?>> BeApproximately(this NumericAssertions<double?> parent, double expectedValue, double precision, string reason,
+            params object[] reasonArgs)
+        {
+            if (parent.Subject == null)
+            {
+                Verification.Fail(
+                    "Expected value <" + parent.Subject + "> to approximate <" + expectedValue + "> +/- {0}{2}, but it was <null>.",
+                    precision, null, reason, reasonArgs);
+            }
+
+            BeApproximately(parent, expectedValue, precision, reason, reasonArgs);
+
+            return new AndConstraint<NumericAssertions<double?>>(parent);
         }
 
         /// <summary>
