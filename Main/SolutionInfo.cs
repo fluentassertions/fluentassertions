c2a8cfc1215f4f170a924ceaaecc3e1c5218089d
dennisdoomen <none@none.com>
2011-04-02 10:34:48 +0000

    Changed the version number to 1.4.0.1

--- Main/SolutionInfo.cs
+++ Main/SolutionInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright Dennis Doomen 2010-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.4.0.1")]
+[assembly: AssemblyFileVersion("1.4.0.1")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
