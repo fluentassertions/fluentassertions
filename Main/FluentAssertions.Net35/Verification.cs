5dc68c3af41e4ddb5b4e4bb81d4cdd7a919f9f93
dennisdoomen <none@none.com>
2011-05-18 20:09:58 +0000

    Internal change so that the Verification class does not prepend "because" when the reason starts with "for"

--- Main/FluentAssertions.Net35/Verification.cs
+++ Main/FluentAssertions.Net35/Verification.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 using FluentAssertions.Formatting;
 using FluentAssertions.Frameworks;
 
 namespace FluentAssertions
 {
-    /// <summary>
-    /// Helper class for verifying a condition and/or throwing a test harness specific exception representing an assertion failure.
-    /// </summary>
-    public static class Verification
+    public class Verification
     {
+        private bool succeeded;
+        private string reason;
+
         /// <summary>
-        /// A list of objects responsible for formatting the objects represented by placeholders.
+        ///   A list of objects responsible for formatting the objects represented by placeholders.
         /// </summary>
         public static readonly List<IValueFormatter> formatters = new List<IValueFormatter>
         {
@@ -26,102 +27,53 @@ namespace FluentAssertions
             new DefaultValueFormatter()
         };
 
-        /// <summary>
-        /// Asserts that the supplied <paramref name = "condition" /> is met.
-        /// </summary>
-        /// <param name = "condition">The condition to assert.</param>
-        /// <param name = "failureMessage">
-        /// The message that will be used in the exception. This should describe what was expected and why. This message 
-        /// can contain the following three placeholders:<br />
-        ///   <list type = "bullet">
-        ///     <item>{0} = the expected value</item>
-        ///     <item>{1} = the actual value</item>
-        ///     <item>{2} = a reason explaining the expectations</item>
-        ///   </list><br/>
-        /// Any additional placeholders are allowed and will be satisfied using the <paramref name="failureMessageArgs"/>.
-        /// </param>
-        /// <param name = "expected">
-        /// The expected value, or <c>null</c> if there is no explicit expected value.
-        /// </param>
-        /// <param name = "actual">The actual value, or <c>null</c> if there is no explicit actual value.</param>
-        /// <param name = "reason">Should describe the reason for the expectation.</param>
-        /// <param name = "reasonArgs">Optional args for formatting placeholders in the <paramref name = "reason" />.</param>
-        /// <param name="failureMessageArgs">
-        /// Optional arguments to satisfy any additional placeholders in the <paramref name="failureMessage"/>
-        /// </param>
-        public static void Verify(Func<bool> condition, string failureMessage, object expected, object actual, string reason,
-            object[] reasonArgs, params object[] failureMessageArgs)
+        internal Verification()
         {
-            Verify(condition.Invoke(), failureMessage, expected, actual, reason, reasonArgs, failureMessageArgs);
         }
 
-        /// <summary>
-        /// Asserts that the supplied <paramref name = "condition" /> is met.
-        /// </summary>
-        /// <param name = "condition">The condition to assert.</param>
-        /// <param name = "failureMessage">
-        /// The message that will be used in the exception. This should describe what was expected and why. This message 
-        /// can contain the following three placeholders:<br />
-        ///   <list type = "bullet">
-        ///     <item>{0} = the expected value</item>
-        ///     <item>{1} = the actual value</item>
-        ///     <item>{2} = a reason explaining the expectations</item>
-        ///   </list><br/>
-        /// Any additional placeholders are allowed and will be satisfied using the <paramref name="failureMessageArgs"/>.
-        /// </param>
-        /// <param name = "expected">
-        /// The expected value, or <c>null</c> if there is no explicit expected value.
-        /// </param>
-        /// <param name = "actual">The actual value, or <c>null</c> if there is no explicit actual value.</param>
-        /// <param name = "reason">Should describe the reason for the expectation.</param>
-        /// <param name = "reasonArgs">Optional args for formatting placeholders in the <paramref name = "reason" />.</param>
-        /// <param name="failureMessageArgs">
-        /// Optional arguments to satisfy any additional placeholders in the <paramref name="failureMessage"/>
-        /// </param>
-        public static void Verify(bool condition, string failureMessage, object expected, object actual, string reason,
-            object[] reasonParameters, params object[] failureMessageArgs)
+        public Verification ForCondition(bool condition)
         {
-            if (!condition)
+            succeeded = condition;
+            return this;
+        }
+
+        public Verification ForCondition(Func<bool> condition)
+        {
+            succeeded = condition();
+            return this;
+        }
+
+        public Verification BecauseOf(string reason, params object[] reasonArgs)
+        {
+            this.reason = SanitizeReason(reason, reasonArgs);
+            return this;
+        }
+
+        private static string SanitizeReason(string reason, object[] reasonArgs)
+        {
+            if (!string.IsNullOrEmpty(reason))
             {
-                Fail(failureMessage, expected, actual, reason, reasonParameters);
+                if (!reason.StartsWith("because", StringComparison.CurrentCultureIgnoreCase) 
+                    && !reason.StartsWith("for ", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    reason = "because " + reason;
+                }
+
+                return " " + String.Format(reason, reasonArgs);
             }
+
+            return "";
         }
 
-        /// <summary>
-        /// Handles an assertion failure.
-        /// </summary>
-        /// <param name = "failureMessage">
-        /// The message that will be used in the exception. This should describe what was expected and why. This message 
-        /// can contain the following three placeholders:<br />
-        ///   <list type = "bullet">
-        ///     <item>{0} = the expected value</item>
-        ///     <item>{1} = the actual value</item>
-        ///     <item>{2} = a reason explaining the expectations</item>
-        ///   </list><br/>
-        /// Any additional placeholders are allowed and will be satisfied using the <paramref name="failureMessageArgs"/>.
-        /// </param>
-        /// <param name = "expected">
-        /// The expected value, or <c>null</c> if there is no explicit expected value.
-        /// </param>
-        /// <param name = "actual">The actual value, or <c>null</c> if there is no explicit actual value.</param>
-        /// <param name = "reason">Should describe the reason for the expectation.</param>
-        /// <param name = "reasonArgs">Optional args for formatting placeholders in the <paramref name = "reason" />.</param>
-        /// <param name="failureMessageArgs">
-        /// Optional arguments to satisfy any additional placeholders in the <paramref name="failureMessage"/>
-        /// </param>
-        public static void Fail(string failureMessage, object expected, object actual, string reason, object[] reasonArgs,
-            params object[] failureMessageArgs)
+        public void FailWith(string failureMessage, params object[] failureArgs)
         {
-            var values = new List<string>
+            if (!succeeded)
             {
-                ToString(expected),
-                ToString(actual),
-                SanitizeReason(reason, reasonArgs),
-            };
+                var values = new List<string>(new[] { reason });
+                values.AddRange(failureArgs.Select(ToString));
 
-            values.AddRange(failureMessageArgs.Select(ToString));
-
-            AssertionHelper.Throw(string.Format(failureMessage, values.ToArray()));
+                AssertionHelper.Throw(string.Format(failureMessage, values.ToArray()));
+            }
         }
 
         /// <summary>
@@ -131,21 +83,6 @@ namespace FluentAssertions
         {
             var formatter = formatters.First(f => f.CanHandle(value));
             return formatter.ToString(value);
-        }
-
-        private static string SanitizeReason(string reason, object[] reasonParameters)
-        {
-            if (!String.IsNullOrEmpty(reason))
-            {
-                if (!reason.StartsWith("because", StringComparison.CurrentCultureIgnoreCase))
-                {
-                    reason = "because " + reason;
-                }
-
-                return " " + String.Format(reason, reasonParameters);
-            }
-
-            return "";
         }
     }
 }
