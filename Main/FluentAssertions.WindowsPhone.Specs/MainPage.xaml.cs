996f80137d283707aa3d9e521a133c229e941c46
ian320 <none@none.com>
2012-01-24 23:24:43 +0000

    Re add Windows Phone version, along with tests. Tests are missing ExceptionAssertionSpecs and ThrowAssertionsSpecs, due to issues with FakeItEasy. Currently 48 failures in the WP version, and 35 failures in the SL version.

---
+++ Main/FluentAssertions.WindowsPhone.Specs/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+using Microsoft.Silverlight.Testing;
+
+namespace FluentAssertions.WindowsPhone.Specs
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var testPage = UnitTestSystem.CreateTestPage() as IMobileTestPage;
+            BackKeyPress += (x, xe) => xe.Cancel = testPage.NavigateBack();
+            (Application.Current.RootVisual as PhoneApplicationFrame).Content = testPage; 
+        }
+    }
+}
