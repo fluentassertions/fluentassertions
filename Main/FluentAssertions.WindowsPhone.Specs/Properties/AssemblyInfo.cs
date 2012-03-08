2f040dca24a629d72a74979cb9acb04094cde86b
dennisdoomen <none@none.com>
2012-03-08 20:00:37 +0000

    Fixed all WP7 and Silverlight specs.

--- Main/FluentAssertions.WindowsPhone.Specs/Properties/AssemblyInfo.cs
+++ Main/FluentAssertions.WindowsPhone.Specs/Properties/AssemblyInfo.cs
@@ -33,3 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("FluentAssertions.WindowsPhone, PublicKey="+
+    "00240000048000009400000006020000002400005253413100040000010001002d25ff515c85b1" + 
+    "3ba08f61d466cff5d80a7f28ba197bbf8796085213e7a3406f970d2a4874932fed35db546e89af" +
+    "2da88c194bf1b7f7ac70de7988c78406f7629c547283061282a825616eb7eb48a9514a75709429" +
+    "36020a9bb37dca9ff60b778309900851575614491c6d25018fadb75828f4c7a17bf2d7dc86e7b6eafc5d8f")]
