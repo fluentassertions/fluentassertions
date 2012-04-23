14762ebeefb89dd38ef485b7a44ba3f9772f707f
mopdam <none@none.com>
2012-04-23 20:43:00 +0000

    Added a separate specs project for the .NET 4.5 assembly

--- Main/FluentAssertions.Net45.Specs/Properties/AssemblyInfo.cs
+++ Main/FluentAssertions.Net45.Specs/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FluentAssertions.Specs")]
+[assembly: AssemblyTitle("FluentAssertions.Net45.Specs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("FluentAssertions.Specs")]
+[assembly: AssemblyProduct("FluentAssertions.Net45.Specs")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f9e79135-800e-4bda-94d9-849a94284131")]
+[assembly: Guid("5beba27d-65b4-4c9b-8a66-bd4a5ee5cd1d")]
