cdc2f88d16bf57c5c8553346f213cba7ef065d66
mopdam <none@none.com>
2012-04-23 19:37:58 +0000

    Added a .NET 4.5 assembly

---
+++ Main/FluentAssertions.Net45/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Fluent Assertions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Fluent Assertions")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c49878ae-d038-4953-a1ee-d2427563a781")]
