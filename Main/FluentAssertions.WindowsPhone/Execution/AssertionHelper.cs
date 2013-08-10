b8d94a2387fa83afb7ebbe89744b16a0d2623a7d
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2013-08-10 21:21:31 +0200

    Added support for WP8 and VS2012 Windows Phone projects.

--- Main/FluentAssertions.WindowsPhone/Execution/AssertionHelper.cs
+++ Main/FluentAssertions.WindowsPhone/Execution/AssertionHelper.cs
@@ -1,20 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FluentAssertions.Execution
 {
-    public class AssertionHelper
+    internal static class AssertionHelper
     {
+        #region Private Definitions
+
+        private static readonly Dictionary<string, ITestFramework> frameworks = new Dictionary<string, ITestFramework>
+        {
+            {"wp75", new WP75TestFramework()},
+            {"wp8", new WP8TestFramework()},
+            {"vs2012", new VS2012TestFramework()},
+        };
+
+        private static ITestFramework testFramework;
+        
+        #endregion
+
         public static void Throw(string message)
         {
-            var testFramework = new WindowsPhoneTestFramework();
-            if (testFramework.IsAvailable)
+            TestFramework.Throw(message);
+        }
+
+        private static ITestFramework TestFramework
+        {
+            get
             {
-                testFramework.Throw(message);
-            }
-            else
-            {
-                throw new InvalidOperationException("Could not find the Windows Phone test framework");
+                if (testFramework == null)
+                {
+                    testFramework = FindStrategy();
+                }
+
+                return testFramework;
             }
         }
+        
+        private static ITestFramework FindStrategy()
+        {
+            ITestFramework detectedFramework = null;
+                detectedFramework = AttemptToDetectUsingAssemblyScanning();
+
+            if (detectedFramework == null)
+            {
+                FailWithIncorrectConfiguration();
+            }
+
+            return detectedFramework;
+        }
+
+        private static void FailWithIncorrectConfiguration()
+        {
+            throw new InvalidOperationException(
+                "Failed to detect the test framework. Make sure that the framework assembly is copied into the test run directory");
+        }
+
+        private static ITestFramework AttemptToDetectUsingAssemblyScanning()
+        {
+            return frameworks.Values.FirstOrDefault(framework => framework.IsAvailable);
+        }
+
     }
 }
