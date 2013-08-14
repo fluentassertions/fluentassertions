eeb66378f6e6135fbee0d9de6cb8a6c841c63596
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2013-08-14 21:19:38 +0200

    Exposed an extension method for obtaining an EventRecorder for a monitored object.

--- Main/FluentAssertions.Net35/EventMonitoringExtensions.cs
+++ Main/FluentAssertions.Net35/EventMonitoringExtensions.cs
@@ -129,7 +129,7 @@ namespace FluentAssertions
         public static EventRecorder ShouldRaise(
             this object eventSource, string eventName, string reason, params object[] reasonArgs)
         {
-            EventRecorder eventRecorder = GetRecorderForEvent(eventSource, eventName);
+            EventRecorder eventRecorder = eventSource.GetRecorderForEvent(eventName);
 
             if (!eventRecorder.Any())
             {
@@ -233,7 +233,7 @@ namespace FluentAssertions
             this T eventSource, Expression<Func<T, object>> propertyExpression,
             string reason, params object[] reasonArgs)
         {
-            EventRecorder eventRecorder = GetRecorderForEvent(eventSource, PropertyChangedEventName);
+            EventRecorder eventRecorder = eventSource.GetRecorderForEvent(PropertyChangedEventName);
             string propertyName = (propertyExpression != null) ? propertyExpression.GetPropertyInfo().Name : null;
 
             if (!eventRecorder.Any())
@@ -282,7 +282,7 @@ namespace FluentAssertions
             this T eventSource, Expression<Func<T, object>> propertyExpression,
             string reason, params object[] reasonArgs)
         {
-            EventRecorder eventRecorder = GetRecorderForEvent(eventSource, PropertyChangedEventName);
+            EventRecorder eventRecorder = eventSource.GetRecorderForEvent(PropertyChangedEventName);
 
             string propertyName = propertyExpression.GetPropertyInfo().Name;
 
@@ -295,7 +295,13 @@ namespace FluentAssertions
             }
         }
 
-        private static EventRecorder GetRecorderForEvent<T>(T eventSource, string eventName)
+        /// <summary>
+        /// Obtains the <see cref="EventRecorder"/> for a particular event of the <paramref name="eventSource"/>.
+        /// </summary>
+        /// <param name="eventSource">The object for which to get an event recorder.</param>
+        /// <param name="eventName">The name of the event.</param>
+        /// <returns></returns>
+        public static EventRecorder GetRecorderForEvent<T>(this T eventSource, string eventName)
         {
             EventRecorder eventRecorder = eventRecordersMap[eventSource].FirstOrDefault(r => r.EventName == eventName);
             if (eventRecorder == null)
