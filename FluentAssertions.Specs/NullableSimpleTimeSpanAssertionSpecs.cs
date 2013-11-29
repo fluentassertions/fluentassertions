45c3b2d45289fc36fc22073172c981798a26078e
Adam Voss <vossad01@gmail.com>
2013-11-29 09:50:16 -0600

    Added `Be` overload to NullableSimpleTimeSpanAssertions.  Allows `Be` assertions against Nullable<TimeSpan> which should fix issue #5.

--- FluentAssertions.Specs/NullableSimpleTimeSpanAssertionSpecs.cs
+++ FluentAssertions.Specs/NullableSimpleTimeSpanAssertionSpecs.cs
@@ -69,6 +69,55 @@ namespace FluentAssertions.Specs
         }
 
         [TestMethod]
+        public void Should_fail_when_asserting_nullable_TimeSpan_value_equals_a_different_value()
+        {
+            TimeSpan? nullableTimeSpanA = 1.Seconds();
+            TimeSpan? nullableTimeSpanB = 2.Seconds();
+
+            Action action = () => nullableTimeSpanA.Should().Be(nullableTimeSpanB);
+
+            action.ShouldThrow<AssertFailedException>();
+        }
+
+        [TestMethod]
+        public void Should_fail_with_descriptive_message_when_asserting_nullable_TimeSpan_null_value_is_equal_to_another_value()
+        {
+            TimeSpan? nullableTimeSpanA = null;
+            TimeSpan? nullableTimeSpanB = 1.Seconds();
+
+            Action action =
+                () =>
+                    nullableTimeSpanA.Should()
+                        .Be(nullableTimeSpanB, "because we want to test the failure {0}", "message");
+
+            action.ShouldThrow<AssertFailedException>()
+                .WithMessage(
+                    "Expected 1s because we want to test the failure message, but found <null>.");
+        }
+
+        [TestMethod]
+        public void Should_succeed_when_asserting_nullable_TimeSpan_null_value_equals_null()
+        {
+            TimeSpan? nullableTimeSpanA = null;
+            TimeSpan? nullableTimeSpanB = null;
+
+            Action action = () => nullableTimeSpanA.Should().Be(nullableTimeSpanB);
+
+            action.ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void Should_succeed_when_asserting_nullable_TimeSpan_value_equals_the_same_value()
+        {
+            TimeSpan? nullableTimeSpanA = new TimeSpan();
+            TimeSpan? nullableTimeSpanB = new TimeSpan();
+
+            Action action = () => nullableTimeSpanA.Should().Be(nullableTimeSpanB);
+
+            action.ShouldNotThrow();
+        }
+
+        [TestMethod]
         public void Should_support_chaining_constraints_with_and()
         {
             TimeSpan? nullableTimeSpan = new TimeSpan();
