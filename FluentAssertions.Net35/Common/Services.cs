0861083ac822b45196bd75037fdeeaa3d64dfbe0
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2013-12-09 20:09:15 +0100

    Added missing specs file.

---
+++ FluentAssertions.Net35/Common/Services.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentAssertions.Common
+{
+    internal static class Services
+    {
+        public static Configuration Configuration { get; set; }
+    }
+}
