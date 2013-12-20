afafbc11390df117667e91781c42f407e0cb800b
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2013-12-20 20:14:29 +0100

    Bumped the version to 2.2

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("www.dennisdoomen.net")]
-[assembly: AssemblyCopyright("Copyright Dennis Doomen 2010-2011")]
+[assembly: AssemblyCopyright("Copyright Dennis Doomen 2010-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyFileVersion("2.2.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
