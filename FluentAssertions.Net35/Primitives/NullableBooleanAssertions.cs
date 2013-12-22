452da26402b703ccf5aa9a3a676ba508f370f5b0
Martin Opdam <martin.opdam@avivasolutions.nl>
2013-12-22 17:03:02 +0100

    Implemented NotBeFalse and NotBeTrue assertions for nullable booleans.

--- FluentAssertions.Net35/Primitives/NullableBooleanAssertions.cs
+++ FluentAssertions.Net35/Primitives/NullableBooleanAssertions.cs
@@ -75,5 +75,45 @@ namespace FluentAssertions.Primitives
 
             return new AndConstraint<BooleanAssertions>(this);
         }
+
+        /// <summary>
+        /// Asserts that the value is not <c>false</c>.
+        /// </summary>
+        /// <param name="reason">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="reasonArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="reason" />.
+        /// </param>
+        public AndConstraint<BooleanAssertions> NotBeFalse(string reason = "", params object[] reasonArgs)
+        {
+            Execute.Assertion
+                .ForCondition(!Subject.HasValue || Subject.Value)
+                .BecauseOf(reason, reasonArgs)
+                .FailWith("Expected nullable boolean not to be {0}{reason}, but found {1}.", false, Subject);
+
+            return new AndConstraint<BooleanAssertions>(this);
+        }
+
+        /// <summary>
+        /// Asserts that the value is not <c>true</c>.
+        /// </summary>
+        /// <param name="reason">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="reasonArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="reason" />.
+        /// </param>
+        public AndConstraint<BooleanAssertions> NotBeTrue(string reason = "", params object[] reasonArgs)
+        {
+            Execute.Assertion
+                .ForCondition(!Subject.HasValue || !Subject.Value)
+                .BecauseOf(reason, reasonArgs)
+                .FailWith("Expected nullable boolean not to be {0}{reason}, but found {1}.", true, Subject);
+
+            return new AndConstraint<BooleanAssertions>(this);
+        }
     }
 }
