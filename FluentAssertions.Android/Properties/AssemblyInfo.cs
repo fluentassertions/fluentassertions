9a8f2725e2baa50aac7f319d30c5c1a7b0251761
Oren Novotny <oren@novotny.org>
2014-01-04 17:27:37 -0500

    Update release builds to put into right dir for nupsec packaging

--- FluentAssertions.Android/Properties/AssemblyInfo.cs
+++ FluentAssertions.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,10 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Fluent Assertions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Fluent Assertions")]
 
-
-
-// Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
 
 [assembly: InternalsVisibleTo("FluentAssertions.Android.Specs")]
