3066fff385a35635062665cda2e2c54156bd0fa4
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2014-01-30 21:11:53 +0100

    Fixed up the WP8 test project so that it uses the correct unit test framework.

---
+++ FluentAssertions.WP8.Specs/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions.WP8.Specs.Resources;
+
+namespace FluentAssertions.WP8.Specs
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
--- FluentAssertions.WP8.Specs/Properties/AssemblyInfo.cs
+++ FluentAssertions.WP8.Specs/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FluentAssertions.WindowsPhone.Specs")]
+[assembly: AssemblyTitle("FluentAssertions.WP8.Specs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FluentAssertions.WindowsPhone.Specs")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FluentAssertions.WP8.Specs")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("625bd6b8-7d54-404d-95d3-0306d1c2285a")]
+[assembly: Guid("df8ee6eb-8ba9-4f83-bf0c-119ae5f2ad70")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,8 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("FluentAssertions.WindowsPhone, PublicKey="+
-    "00240000048000009400000006020000002400005253413100040000010001002d25ff515c85b1" + 
-    "3ba08f61d466cff5d80a7f28ba197bbf8796085213e7a3406f970d2a4874932fed35db546e89af" +
-    "2da88c194bf1b7f7ac70de7988c78406f7629c547283061282a825616eb7eb48a9514a75709429" +
-    "36020a9bb37dca9ff60b778309900851575614491c6d25018fadb75828f4c7a17bf2d7dc86e7b6eafc5d8f")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
