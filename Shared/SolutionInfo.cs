a2f1638cfa493b04d0fa36e3beb71cda52f3c69e
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2014-07-16 20:55:59 +0200

    Updated the OTHER solutioninfo.cs to 3.1

--- Shared/SolutionInfo.cs
+++ Shared/SolutionInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright Dennis Doomen 2010-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.0.9999")]
-[assembly: AssemblyFileVersion("3.0.9999")]
+[assembly: AssemblyVersion("3.1.9999")]
+[assembly: AssemblyFileVersion("3.1.9999")]
