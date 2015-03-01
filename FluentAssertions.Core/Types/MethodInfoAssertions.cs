911c1a9ed03dee04677a117f687b349f88b4a6ad
piotrpMSFT <piotrp@microsoft.com>
2015-03-01 15:16:53 -0800

    Moved MethodInfoAssertions onto MemberInfoAssertions base class

--- FluentAssertions.Core/Types/MethodInfoAssertions.cs
+++ FluentAssertions.Core/Types/MethodInfoAssertions.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 using FluentAssertions.Execution;
-using FluentAssertions.Primitives;
 
 namespace FluentAssertions.Types
 {
@@ -15,60 +11,11 @@ namespace FluentAssertions.Types
     /// </summary>
     [DebuggerNonUserCode]
     public class MethodInfoAssertions :
-        ReferenceTypeAssertions<MethodInfo, MethodInfoAssertions>
+        MemberInfoAssertions<MethodInfo, MethodInfoAssertions>
     {
         public MethodInfoAssertions(MethodInfo methodInfo)
         {
             Subject = methodInfo;
-        }
-
-        /// <summary>
-        /// Asserts that the selected method is decorated with the specified <typeparamref name="TAttribute"/>.
-        /// </summary>
-        /// <param name="because">
-        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
-        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
-        /// </param>
-        /// <param name="reasonArgs">
-        /// Zero or more objects to format using the placeholders in <see cref="because" />.
-        /// </param>
-        public AndWhichConstraint<MethodInfoAssertions, TAttribute> BeDecoratedWith<TAttribute>(
-            string because = "", params object[] reasonArgs)
-            where TAttribute : Attribute
-        {
-            return BeDecoratedWith<TAttribute>(attr => true, because, reasonArgs);
-        }
-
-        /// <summary>
-        /// Asserts that the selected method is decorated with an attribute of type <typeparamref name="TAttribute"/>
-        /// that matches the specified <paramref name="isMatchingAttributePredicate"/>.
-        /// </summary>
-        /// <param name="isMatchingAttributePredicate">
-        /// The predicate that the attribute must match.
-        /// </param>
-        /// <param name="because">
-        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
-        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
-        /// </param>
-        /// <param name="reasonArgs">
-        /// Zero or more objects to format using the placeholders in <see cref="because" />.
-        /// </param>
-        public AndWhichConstraint<MethodInfoAssertions, TAttribute> BeDecoratedWith<TAttribute>(
-            Expression<Func<TAttribute, bool>> isMatchingAttributePredicate,
-            string because = "", params object[] reasonArgs)
-            where TAttribute : Attribute
-        {
-            string failureMessage = "Expected method " + GetDescriptionFor(Subject) +
-                " to be decorated with {0}{reason}, but that attribute was not found.";
-
-            IEnumerable<TAttribute> attributes = GetAttributesFor(Subject, isMatchingAttributePredicate);
-
-            Execute.Assertion
-                .ForCondition(attributes.Any())
-                .BecauseOf(because, reasonArgs)
-                .FailWith(failureMessage, typeof(TAttribute));
-
-            return new AndWhichConstraint<MethodInfoAssertions, TAttribute>(this, attributes);
         }
 
         /// <summary>
@@ -85,7 +32,7 @@ namespace FluentAssertions.Types
             string because = "", params object[] reasonArgs)
         {
             string failureMessage = "Expected method " +
-                                    GetDescriptionFor(Subject) +
+                                    SubjectDescription +
                                     " to be virtual{reason}, but it is not virtual.";
 
             Execute.Assertion
@@ -107,11 +54,11 @@ namespace FluentAssertions.Types
         public AndConstraint<MethodInfoAssertions> BeAsync(string because = "", params object[] reasonArgs)
         {
             string failureMessage = "Expected subject " +
-                        GetDescriptionFor(Subject) +
+                        SubjectDescription +
                         " to be async{reason}, but it is not async.";
 
             Execute.Assertion
-                .ForCondition(!IsNonAsync(Subject))
+                .ForCondition(!IsNonAsync())
                 .BecauseOf(because, reasonArgs)
                 .FailWith(failureMessage);
 
@@ -126,14 +73,9 @@ namespace FluentAssertions.Types
                 method.DeclaringType, method.Name);
         }
 
-        internal static IEnumerable<TAttribute> GetAttributesFor<TAttribute>(MethodInfo subject, Expression<Func<TAttribute, bool>> isMatchingAttributePredicate) where TAttribute : Attribute
+        internal override string SubjectDescription
         {
-            IEnumerable<TAttribute> attributes = subject.GetCustomAttributes(
-                typeof(TAttribute), false)
-                .Cast<TAttribute>()
-                .Where(isMatchingAttributePredicate.Compile());
-
-            return attributes;
+            get { return GetDescriptionFor(Subject); }
         }
 
         internal static bool IsNonVirtual(MethodInfo method)
@@ -141,9 +83,9 @@ namespace FluentAssertions.Types
             return !method.IsVirtual || method.IsFinal;
         }
 
-        internal static bool IsNonAsync(MethodInfo subject)
+        internal bool IsNonAsync()
         {
-            return !GetAttributesFor<Attribute>(subject, a => a.GetType().FullName.Equals("System.Runtime.CompilerServices.AsyncStateMachineAttribute")).Any();
+            return !GetMatchingAttributes<Attribute>(a => a.GetType().FullName.Equals("System.Runtime.CompilerServices.AsyncStateMachineAttribute")).Any();
         }
 
         protected override string Context
