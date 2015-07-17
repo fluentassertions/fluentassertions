7d8912c07c3d14c3b1f4762386698caad5d04fe4
Oren Novotny <oren@novotny.org>
2015-07-17 13:18:43 -0400

    Add in test frameworks

---
+++ FluentAssertions.Dotnet/Execution/MSTestFramework.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentAssertions.Execution
+{
+    internal class MSTestFramework : LateBoundTestFramework
+    {
+        protected override string AssemblyName
+        {
+            get { return "Microsoft.VisualStudio.TestPlatform.UnitTestFramework"; }
+        }
+
+        protected override string ExceptionFullName
+        {
+            get { return "Microsoft.VisualStudio.TestPlatform.UnitTestFramework.AssertFailedException"; }
+        }
+    }
+}
