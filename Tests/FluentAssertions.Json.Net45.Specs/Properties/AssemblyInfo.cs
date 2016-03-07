2d21e59f06de2e0e0971e055efca4bf729bfaab1
mkoertgen <marcel.koertgen@gmail.com>
2016-03-07 15:34:21 +0100

    Added Json project & specs

---
+++ Tests/FluentAssertions.Json.Net45.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FluentAssertions.Json.Net45.Specs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("FluentAssertions.Json.Net45.Specs")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63a1554c-db57-463a-9df4-2748eb403dd9")]
