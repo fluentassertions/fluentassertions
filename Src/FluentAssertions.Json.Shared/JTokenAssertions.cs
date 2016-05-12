97240985da5d465b556e24cd752fe1abc694bb32
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2016-05-12 21:55:26 +0200

    Added Json project & specs (#403)

---
+++ Src/FluentAssertions.Json.Shared/JTokenAssertions.cs
@@ -0,0 +1,243 @@
+﻿using System.Diagnostics;
+using FluentAssertions.Common;
+using FluentAssertions.Execution;
+using FluentAssertions.Formatting;
+using FluentAssertions.Primitives;
+using Newtonsoft.Json.Linq;
+
+namespace FluentAssertions.Json
+{
+    /// <summary>
+    ///     Contains a number of methods to assert that an <see cref="JToken" /> is in the expected state.
+    /// </summary>
+    [DebuggerNonUserCode]
+    public class JTokenAssertions : ReferenceTypeAssertions<JToken, JTokenAssertions>
+    {
+        static JTokenAssertions()
+        {
+            Formatter.AddFormatter(new JTokenFormatter());
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="JTokenAssertions" /> class.
+        /// </summary>
+        /// <param name="subject">The subject</param>
+        public JTokenAssertions(JToken subject)
+        {
+            Subject = subject;
+        }
+
+        /// <summary>
+        ///     Returns the type of the subject the assertion applies on.
+        /// </summary>
+#if !PORTABLE && !CORE_CLR
+        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+#endif
+        protected override string Context => nameof(JToken);
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> equals the <paramref name="expected" /> element.
+        /// </summary>
+        /// <param name="expected">The expected element</param>
+        public AndConstraint<JTokenAssertions> Be(JToken expected)
+        {
+            return Be(expected, string.Empty);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> equals the <paramref name="expected" /> element.
+        /// </summary>
+        /// <param name="expected">The expected element</param>
+        /// <param name="because">
+        ///     A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion
+        ///     is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        ///     Zero or more objects to format using the placeholders in <see paramref="because" />.
+        /// </param>
+        public AndConstraint<JTokenAssertions> Be(JToken expected, string because, params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .ForCondition(JToken.DeepEquals(Subject, expected))
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected JSON document to be {0}{reason}, but found {1}.", expected, Subject);
+
+            return new AndConstraint<JTokenAssertions>(this);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> does not equal the <paramref name="unexpected" /> element,
+        ///     using its <see cref="object.Equals(object)" /> implementation.
+        /// </summary>
+        /// <param name="unexpected">The unexpected element</param>
+        public AndConstraint<JTokenAssertions> NotBe(JToken unexpected)
+        {
+            return NotBe(unexpected, string.Empty);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> does not equal the <paramref name="unexpected" /> element,
+        ///     using its <see cref="object.Equals(object)" /> implementation.
+        /// </summary>
+        /// <param name="unexpected">The unexpected element</param>
+        /// <param name="because">
+        ///     A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion
+        ///     is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        ///     Zero or more objects to format using the placeholders in <see paramref="because" />.
+        /// </param>
+        public AndConstraint<JTokenAssertions> NotBe(JToken unexpected, string because, params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .ForCondition((ReferenceEquals(Subject, null) && !ReferenceEquals(unexpected, null)) ||
+                              !JToken.DeepEquals(Subject, unexpected))
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected JSON document not to be {0}{reason}.", unexpected);
+
+            return new AndConstraint<JTokenAssertions>(this);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> is equivalent to the <paramref name="expected" /> element,
+        ///     using its <see cref="JToken.DeepEquals(JToken, JToken)" /> implementation.
+        /// </summary>
+        /// <param name="expected">The expected element</param>
+        /// <remarks>
+        /// Json tokens are compared by inspecting all properties. They are considered equal
+        /// when all property names and values match (independent of their order). 
+        /// When not equal, the first mismatching property or value is included in the assertion failure message.
+        /// </remarks>
+        public AndConstraint<JTokenAssertions> BeEquivalentTo(JToken expected)
+        {
+            return BeEquivalentTo(expected, string.Empty);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> is equivalent to the <paramref name="expected" /> element,
+        ///     using its <see cref="JToken.DeepEquals(JToken, JToken)" /> implementation.
+        /// </summary>
+        /// <param name="expected">The expected element</param>
+        /// <param name="because">
+        ///     A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion
+        ///     is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        ///     Zero or more objects to format using the placeholders in <see paramref="because" />.
+        /// </param>
+        public AndConstraint<JTokenAssertions> BeEquivalentTo(JToken expected, string because,
+            params object[] becauseArgs)
+        {
+            ObjectDiffPatchResult diff = ObjectDiffPatch.GenerateDiff(Subject, expected);
+            JToken firstDifferingToken = diff.NewValues?.First ?? diff.OldValues?.First;
+
+            Execute.Assertion
+                .ForCondition(diff.AreEqual)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected JSON document {0} to be equivalent to {1}{reason}, but differs at {2}.",
+                    Subject, expected, firstDifferingToken);
+
+            return new AndConstraint<JTokenAssertions>(this);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> is not equivalent to the <paramref name="unexpected" /> element,
+        ///     using its <see cref="JToken.DeepEquals(JToken, JToken)" /> implementation.
+        /// </summary>
+        /// <param name="unexpected">The unexpected element</param>
+        public AndConstraint<JTokenAssertions> NotBeEquivalentTo(JToken unexpected)
+        {
+            return NotBeEquivalentTo(unexpected, string.Empty);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> is not equivalent to the <paramref name="unexpected" /> element,
+        ///     using its <see cref="JToken.DeepEquals(JToken, JToken)" /> implementation.
+        /// </summary>
+        /// <param name="unexpected">The unexpected element</param>
+        /// <param name="because">
+        ///     A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion
+        ///     is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        ///     Zero or more objects to format using the placeholders in <see paramref="because" />.
+        /// </param>
+        public AndConstraint<JTokenAssertions> NotBeEquivalentTo(JToken unexpected, string because,
+            params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .ForCondition(!JToken.DeepEquals(Subject, unexpected))
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Did not expect JSON element {0} to be equivalent to {1}{reason}.",
+                    Subject, unexpected);
+
+            return new AndConstraint<JTokenAssertions>(this);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> has the specified <paramref name="expected" /> value.
+        /// </summary>
+        /// <param name="expected">The expected value</param>
+        public AndConstraint<JTokenAssertions> HaveValue(string expected)
+        {
+            return HaveValue(expected, string.Empty);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> has the specified <paramref name="expected" /> value.
+        /// </summary>
+        /// <param name="expected">The expected value</param>
+        /// <param name="because">
+        ///     A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion
+        ///     is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        ///     Zero or more objects to format using the placeholders in <see paramref="because" />.
+        /// </param>
+        public AndConstraint<JTokenAssertions> HaveValue(string expected, string because, params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .ForCondition(Subject.Value<string>() == expected)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected JSON property {0} to have value {1}{reason}, but found {2}.",
+                    Subject.Path, expected, Subject.Value<string>());
+
+            return new AndConstraint<JTokenAssertions>(this);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> has a direct child element with the specified
+        ///     <paramref name="expected" /> name.
+        /// </summary>
+        /// <param name="expected">The name of the expected child element</param>
+        public AndWhichConstraint<JTokenAssertions, JToken> HaveElement(string expected)
+        {
+            return HaveElement(expected, string.Empty);
+        }
+
+        /// <summary>
+        ///     Asserts that the current <see cref="JToken" /> has a direct child element with the specified
+        ///     <paramref name="expected" /> name.
+        /// </summary>
+        /// <param name="expected">The name of the expected child element</param>
+        /// <param name="because">
+        ///     A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion
+        ///     is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        ///     Zero or more objects to format using the placeholders in <see paramref="because" />.
+        /// </param>
+        public AndWhichConstraint<JTokenAssertions, JToken> HaveElement(string expected, string because,
+            params object[] becauseArgs)
+        {
+            JToken jToken = Subject[expected];
+            Execute.Assertion
+                .ForCondition(jToken != null)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected JSON document {0} to have element \"" + expected.Escape(true) + "\"{reason}" +
+                          ", but no such element was found.", Subject);
+
+            return new AndWhichConstraint<JTokenAssertions, JToken>(this, jToken);
+        }
+    }
+}
---
+++ Src/FluentAssertions.Json.Shared/ObjectDiffPatch.cs
@@ -0,0 +1,360 @@
+﻿#region *   License     *
+/*
+    SimpleHelpers - ObjectDiffPatch   
+
+    Copyright © 2014 Khalid Salomão
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the “Software”), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE. 
+
+    License: http://www.opensource.org/licenses/mit-license.php
+    Website: https://github.com/khalidsalomao/SimpleHelpers.Net
+ */
+
+#endregion
+
+ // Note mkoertgen: copied from original repo. Made it internal, removed non-needed code and added tests.
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+#if CORE_CLR
+using System.Reflection;
+#endif
+
+namespace FluentAssertions.Json
+{
+    [DebuggerNonUserCode]
+    internal static class ObjectDiffPatch
+    {
+        private const string PrefixArraySize = "@@ Count";
+        private const string PrefixRemovedFields = "@@ Removed";
+
+        /// <summary>
+        /// Compares two objects and generates the differences between them.
+        /// </summary>
+        /// <typeparam name="T">The type of the T.</typeparam>
+        /// <param name="original">The original.</param>
+        /// <param name="updated">The updated.</param>
+        /// <returns></returns>
+        public static ObjectDiffPatchResult GenerateDiff<T>(T original, T updated) where T : class
+        {
+            // ensure the serializer will not ignore null values
+            var writer = GetJsonSerializer();
+            // parse our objects
+            JObject originalJson, updatedJson;
+            if (typeof(JObject).IsAssignableFrom(typeof(T)))
+            {
+                originalJson = original as JObject;
+                updatedJson = updated as JObject;
+            }
+            else
+            {
+                originalJson = original != null ? JObject.FromObject(original, writer) : null;
+                updatedJson = updated != null ? JObject.FromObject(updated, writer) : null;
+            }
+            // analyse their differences!
+            var result = Diff(originalJson, updatedJson);
+            result.Type = typeof(T);
+            return result;
+        }
+
+        /// <summary>
+        /// Modifies an object according to a diff.
+        /// </summary>
+        /// <typeparam name="T">The type of the T.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="diffJson">The diff json.</param>
+        /// <returns></returns>
+        public static T PatchObject<T>(T source, string diffJson) where T : class
+        {
+            var diff = JObject.Parse(diffJson);
+            return PatchObject(source, diff);
+        }
+
+        /// <summary>
+        /// Modifies an object according to a diff.
+        /// </summary>
+        /// <typeparam name="T">The type of the T.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="diffJson">The diff json.</param>
+        /// <returns></returns>
+        public static T PatchObject<T>(T source, JObject diffJson) where T : class
+        {
+            var sourceJson = source != null ? JObject.FromObject(source, GetJsonSerializer()) : null;
+            var resultJson = Patch(sourceJson, diffJson);
+
+            return resultJson?.ToObject<T>();
+        }
+
+        private static ObjectDiffPatchResult Diff(JObject source, JObject target)
+        {
+            var result = new ObjectDiffPatchResult();
+            // check for null values
+            if (source == null && target == null)
+            {
+                return result;
+            }
+
+            if (source == null || target == null)
+            {
+                result.OldValues = source;
+                result.NewValues = target;
+                return result;
+            }
+
+            // compare internal fields           
+            var removedNew = new JArray();
+            var removedOld = new JArray();
+            JToken token;
+            // start by iterating in source fields
+            foreach (var i in source)
+            {
+                // check if field exists
+                if (!target.TryGetValue(i.Key, out token))
+                {
+                    AddOldValuesToken(result, i.Value, i.Key);
+                    removedNew.Add(i.Key);
+                }
+                // compare field values
+                else
+                {
+                    DiffField(i.Key, i.Value, token, result);
+                }
+            }
+            // then iterate in target fields that are not present in source
+            foreach (var i in target)
+            {
+                // ignore alredy compared values
+                if (source.TryGetValue(i.Key, out token))
+                    continue;
+                // add missing tokens
+                removedOld.Add(i.Key);
+                AddNewValuesToken(result, i.Value, i.Key);
+            }
+
+            if (removedOld.Count > 0)
+                AddOldValuesToken(result, removedOld, PrefixRemovedFields);
+            if (removedNew.Count > 0)
+                AddNewValuesToken(result, removedNew, PrefixRemovedFields);
+
+            return result;
+        }
+
+        internal static void DiffField(string fieldName, JToken source, JToken target, ObjectDiffPatchResult result)
+        {
+            if (source == null)
+            {
+                if (target != null)
+                {
+                    AddToken(result, fieldName, null, target);
+                }
+            }
+            else if (target == null)
+            {
+                AddToken(result, fieldName, source, null);
+            }
+            else if (source.Type == JTokenType.Object)
+            {
+                var v = target as JObject;
+                var r = Diff(source as JObject, v);
+                if (!r.AreEqual)
+                    AddToken(result, fieldName, r);
+            }
+            else if (source.Type == JTokenType.Array)
+            {
+                var aS = (JArray) source;
+                var aT = (JArray) target;
+
+                if ((aS.Count == 0 || aT.Count == 0) && (aS.Count != aT.Count))
+                {
+                    AddToken(result, fieldName, source, target);
+                }
+                else
+                {
+                    var arrayDiff = new ObjectDiffPatchResult();
+                    var minCount = Math.Min(aS.Count, aT.Count);
+                    for (var i = 0; i < Math.Max(aS.Count, aT.Count); i++)
+                    {
+                        if (i < minCount)
+                        {
+                            DiffField(i.ToString(), aS[i], aT[i], arrayDiff);
+                        }
+                        else if (i >= aS.Count)
+                        {
+                            AddNewValuesToken(arrayDiff, aT[i], i.ToString());
+                        }
+                        else
+                        {
+                            AddOldValuesToken(arrayDiff, aS[i], i.ToString());
+                        }
+                    }
+
+                    if (!arrayDiff.AreEqual)
+                    {
+                        if (aS.Count != aT.Count)
+                            AddToken(arrayDiff, PrefixArraySize, aS.Count, aT.Count);
+                        AddToken(result, fieldName, arrayDiff);
+                    }
+                }
+            }
+            else
+            {
+                if (!JToken.DeepEquals(source, target))
+                {
+                    AddToken(result, fieldName, source, target);
+                }
+            }
+        }
+
+        private static JsonSerializer GetJsonSerializer()
+        {
+            // ensure the serializer will not ignore null values
+            var settings = JsonConvert.DefaultSettings != null ? JsonConvert.DefaultSettings() : new JsonSerializerSettings();
+            settings.NullValueHandling = NullValueHandling.Include;
+            settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            settings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            settings.Formatting = Newtonsoft.Json.Formatting.None;
+            settings.MissingMemberHandling = MissingMemberHandling.Ignore;
+            settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+
+            // create our custom serializer
+            var writer = JsonSerializer.Create(settings);
+            return writer;
+        }
+
+        private static JToken Patch(JToken sourceJson, JToken diffJson)
+        {
+            // deal with null values
+            if (sourceJson == null || diffJson == null || !sourceJson.HasValues)
+                return diffJson;
+
+            if (diffJson.Type != JTokenType.Object)
+                return diffJson;
+
+            // deal with objects
+            var diffObj = (JObject)diffJson;
+            JToken token;
+            if (sourceJson.Type == JTokenType.Array)
+            {
+                var sz = 0;
+                var foundArraySize = diffObj.TryGetValue(PrefixArraySize, out token);
+                if (foundArraySize)
+                {
+                    diffObj.Remove(PrefixArraySize);
+                    sz = token.Value<int>();
+                }
+                var array = (JArray) sourceJson;
+                // resize array
+                if (foundArraySize && array.Count != sz)
+                {
+                    var snapshot = (JArray) array.DeepClone();
+                    array.Clear();
+                    for (var i = 0; i < sz; i++)
+                    {
+                        array.Add(i < snapshot.Count ? snapshot[i] : null);
+                    }
+                }
+                // patch it
+                foreach (var f in diffObj)
+                {
+                    int ix;
+                    if (int.TryParse(f.Key, out ix))
+                    {
+                        array[ix] = Patch(array[ix], f.Value);
+                    }
+                }
+            }
+            else
+            {
+                var sourceObj = sourceJson as JObject ?? new JObject();
+                // remove fields
+                if (diffObj.TryGetValue(PrefixRemovedFields, out token))
+                {
+                    diffObj.Remove(PrefixRemovedFields);
+                    foreach (var f in (JArray) token)
+                        sourceObj.Remove(f.ToString());
+                }
+
+                // patch it
+                foreach (var f in diffObj)
+                {
+                    sourceObj[f.Key] = Patch(sourceObj[f.Key], f.Value);
+                }
+            }
+            return sourceJson;
+        }
+
+        private static void AddNewValuesToken(ObjectDiffPatchResult item, JToken newToken, string fieldName)
+        {
+            if (item.NewValues == null)
+                item.NewValues = new JObject();
+            item.NewValues[fieldName] = newToken;
+        }
+
+        private static void AddOldValuesToken(ObjectDiffPatchResult item, JToken oldToken, string fieldName)
+        {
+            if (item.OldValues == null)
+                item.OldValues = new JObject();
+            item.OldValues[fieldName] = oldToken;
+        }
+
+        private static void AddToken(ObjectDiffPatchResult item, string fieldName, JToken oldToken, JToken newToken)
+        {
+            AddOldValuesToken(item, oldToken, fieldName);
+            AddNewValuesToken(item, newToken, fieldName);
+        }
+
+        private static void AddToken(ObjectDiffPatchResult item, string fieldName, ObjectDiffPatchResult diff)
+        {
+            AddToken(item, fieldName, diff.OldValues, diff.NewValues);
+        }
+    }
+
+    /// <summary>
+    /// Result of a diff operation between two objects
+    /// </summary>
+    public class ObjectDiffPatchResult
+    {
+        /// <summary>
+        /// If the compared objects are equal.
+        /// </summary>
+        /// <value>true if the obects are equal; otherwise, false.</value>
+        public bool AreEqual => OldValues == null && NewValues == null;
+
+        /// <summary>
+        /// The type of the compared objects.
+        /// </summary>
+        /// <value>The type of the compared objects.</value>
+        public Type Type { get; set; }
+
+        /// <summary>
+        /// The values modified in the original object.
+        /// </summary>
+        public JObject OldValues { get; set; }
+
+        /// <summary>
+        /// The values modified in the updated object.
+        /// </summary>
+        public JObject NewValues { get; set; }
+    }
+}
