f5303c25bacc95667660754cf350616cea19cc2b
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2016-06-05 02:34:01 -0500

    #366 support for NotBeOfType (#413)
    * Added NotBeOfType methods and associated tests
    
    * Fixed indentation
    
    * Replace `type` with `{context:type}`
    
    * Removed null check since a null value would be by definition, not of the given type
    
    * Added test for `BeOfType` when under test value is null and fixed NotBeOfType tests
    
    * Added null assertion for NotBeOfType and associated tests

--- Src/Core/Primitives/ReferenceTypeAssertions.cs
+++ Src/Core/Primitives/ReferenceTypeAssertions.cs
@@ -121,6 +121,24 @@ namespace FluentAssertions.Primitives
         }
 
         /// <summary>
+        /// Asserts that the object is not of the specified type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type that the subject is not supposed to be of.</typeparam>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="because" />.
+        /// </param>
+        public AndWhichConstraint<TAssertions, T> NotBeOfType<T>(string because = "", params object[] becauseArgs)
+        {
+            NotBeOfType(typeof(T), because, becauseArgs);
+
+            return new AndWhichConstraint<TAssertions, T>((TAssertions)this, (T)(object)Subject);
+        }
+
+        /// <summary>
         /// Asserts that the object is of the specified type <paramref name="expectedType"/>.
         /// </summary>
         /// <param name="expectedType">
@@ -138,9 +156,34 @@ namespace FluentAssertions.Primitives
             Execute.Assertion
                 .ForCondition(!ReferenceEquals(Subject, null))
                 .BecauseOf(because, becauseArgs)
-                .FailWith("Expected type to be {0}{reason}, but found <null>.", expectedType);
+                .FailWith("Expected {context:type} to be {0}{reason}, but found <null>.", expectedType);
 
             Subject.GetType().Should().Be(expectedType, because, becauseArgs);
+
+            return new AndConstraint<TAssertions>((TAssertions)this);
+        }
+
+        /// <summary>
+        /// Asserts that the object is not of the specified type <paramref name="expectedType"/>.
+        /// </summary>
+        /// <param name="expectedType">
+        /// The type that the subject is not supposed to be of.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="because" />.
+        /// </param>
+        public AndConstraint<TAssertions> NotBeOfType(Type expectedType, string because = "", params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .ForCondition(!ReferenceEquals(Subject, null))
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected {context:type} not to be {0}{reason}, but found <null>.", expectedType);
+
+            Subject.GetType().Should().NotBe(expectedType, because, becauseArgs);
 
             return new AndConstraint<TAssertions>((TAssertions)this);
         }
