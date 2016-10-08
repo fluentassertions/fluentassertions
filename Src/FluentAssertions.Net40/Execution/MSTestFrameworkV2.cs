5a2e4fd2af62a44f96de6f858c4790c30abe170e
Abhitej Anoop John Bandi <aajohn@microsoft.com>
2016-10-09 02:52:57 +0530

    Adding in support for MSTest V2 which is the next version of the MSTest framework. It supports .Net 4.5+, UWP and dotnet core.

---
+++ Src/FluentAssertions.Net40/Execution/MSTestFrameworkV2.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentAssertions.Execution
+{
+    internal class MSTestFrameworkV2 : LateBoundTestFramework
+    {
+        protected override string ExceptionFullName
+        {
+            get { return "Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException"; }
+        }
+
+        protected override string AssemblyName
+        {
+            get { return "Microsoft.VisualStudio.TestPlatform.TestFramework"; }
+        }
+    }
+}
