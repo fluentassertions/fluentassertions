10e5bdb3e382f81785133dc79258d50cd68df647
Abhitej John <abhitej@outlook.com>
2016-10-09 12:31:18 +0530

    Adding in support for MSTest V2 which is the next version of the MSTest framework. It supports .Net 4.5+, UWP and dotnet core. (#486)

---
+++ Tests/TestFrameworks/MSTestV2.Specs/FrameworkSpecs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestV2.Specs
+{
+    [TestClass]
+    public class FrameworkSpecs
+    {
+        [TestMethod]
+        public void When_mstestv2_is_used_it_should_throw_mstest_exceptions_for_assertion_failures()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            Action act = () => 0.Should().Be(1);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            Exception exception = act.ShouldThrow<Exception>().Which;
+            exception.GetType().FullName.Should().ContainEquivalentOf("Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException");
+        }
+    }
+}
