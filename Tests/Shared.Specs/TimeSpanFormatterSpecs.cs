172fd3f187d310e1ee016139df5a96628da7add8
ChristopherHaws <cribs2@gmail.com>
2017-10-08 17:52:22 -0700

    Add formatting for more precise DateTime, DateTimeOffset, and TimeSpan's

--- Tests/Shared.Specs/TimeSpanFormatterSpecs.cs
+++ Tests/Shared.Specs/TimeSpanFormatterSpecs.cs
@@ -1,92 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using FluentAssertions.Formatting;
 using Xunit;
 
 namespace FluentAssertions.Specs
 {
-    
     public class TimeSpanFormatterSpecs
     {
         [Fact]
-        public void When_rendering_empty_time_span_to_string_it_should_return_a_literal()
-        {
-            When_rendering_a_time_span_it_should_include_the_non_zero_parts_of(new TimeSpan(), "default");
-        }
-
-        [Fact]
-        public void When_rendering_time_spans_to_string_it_should_only_include_the_nonzero_parts()
+        public void When_zero_time_span_it_should_return_a_literal()
         {
             //-----------------------------------------------------------------------------------------------------------
             // Arrange
             //-----------------------------------------------------------------------------------------------------------
-            var testCases = new[]
-            {
-               new { Input = TimeSpan.FromDays(2), Output = "2d" },
-               new { Input = new TimeSpan(0, 3, 20, 10, 58), Output = "3h, 20m and 10.058s" },
-               new { Input = new TimeSpan(2, 0, 0, 12, 0), Output = "2d and 12s" },
-               new { Input = TimeSpan.FromSeconds(90), Output = "1m and 30s" },
-               new { Input = TimeSpan.FromMilliseconds(10), Output = "0.010s" },
-               new { Input = TimeSpan.MinValue, Output = "min time span" },
-               new { Input = TimeSpan.MaxValue, Output = "max time span" },
-               new { Input = TimeSpan.FromTicks(1), Output = "0.1us" },
-               new { Input = TimeSpan.FromTicks(TimeSpan.TicksPerMillisecond - 1), Output = "999.9us" }
-            };
-
-            //-----------------------------------------------------------------------------------------------------------
-            // Act
-            //-----------------------------------------------------------------------------------------------------------
-            foreach (var testCase in testCases)
-            {
-                When_rendering_a_time_span_it_should_include_the_non_zero_parts_of(testCase.Input, testCase.Output);
-            }
-        }
-
-        [Fact]
-        public void When_rendering_negative_time_spans_to_string_it_should_only_include_the_nonzero_parts()
-        {
-            //-----------------------------------------------------------------------------------------------------------
-            // Arrange
-            //-----------------------------------------------------------------------------------------------------------
-            var testCases = new[]
-            {
-               new { Input = TimeSpan.FromDays(2).Negate(), Output = "-2d" },
-               new { Input = new TimeSpan(0, 3, 20, 10, 58).Negate(), Output = "-3h, 20m and 10.058s" },
-               new { Input = new TimeSpan(2, 0, 0, 12, 0).Negate(), Output = "-2d and 12s" },
-               new { Input = TimeSpan.FromSeconds(90).Negate(), Output = "-1m and 30s" },
-               new { Input = TimeSpan.FromMilliseconds(10).Negate(), Output = "-0.010s" },
-               new { Input = TimeSpan.FromTicks(1).Negate(), Output = "-0.1us" },
-               new { Input = TimeSpan.FromTicks(TimeSpan.TicksPerMillisecond - 1).Negate(), Output = "-999.9us" }
-            };
-
-            //-----------------------------------------------------------------------------------------------------------
-            // Act
-            //-----------------------------------------------------------------------------------------------------------
-            foreach (var testCase in testCases)
-            {
-                When_rendering_a_time_span_it_should_include_the_non_zero_parts_of(testCase.Input, testCase.Output);
-            }
-        }
-
-        private static void When_rendering_a_time_span_it_should_include_the_non_zero_parts_of(TimeSpan timeSpan, string expectedOutput)
-        {
-            //-----------------------------------------------------------------------------------------------------------
-            // Arrange
-            //-----------------------------------------------------------------------------------------------------------            
             var formatter = new TimeSpanValueFormatter();
 
             //-----------------------------------------------------------------------------------------------------------
             // Act
             //-----------------------------------------------------------------------------------------------------------
-            var result = formatter.Format(timeSpan, new FormattingContext(), null);
+            string result = formatter.Format(TimeSpan.Zero, new FormattingContext(), null);
 
             //-----------------------------------------------------------------------------------------------------------
             // Assert
             //-----------------------------------------------------------------------------------------------------------
-            result.Should().Be(expectedOutput);
+            result.Should().Be("default");
+        }
+
+        [Fact]
+        public void When_max_time_span_it_should_return_a_literal()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var formatter = new TimeSpanValueFormatter();
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            string result = formatter.Format(TimeSpan.MaxValue, new FormattingContext(), null);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.Should().Be("max time span");
+        }
+
+        [Fact]
+        public void When_min_time_span_it_should_return_a_literal()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var formatter = new TimeSpanValueFormatter();
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            string result = formatter.Format(TimeSpan.MinValue, new FormattingContext(), null);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.Should().Be("min time span");
+        }
+
+        
+        [Theory]
+        [InlineData("00:00:00.0000007", "0.7µs")]
+        [InlineData("-00:00:00.0000007", "-0.7µs")]
+        [InlineData("00:00:00.000456", "456.0µs")]
+        [InlineData("-00:00:00.000456", "-456.0µs")]
+        [InlineData("00:00:00.0004567", "456.7µs")]
+        [InlineData("-00:00:00.0004567", "-456.7µs")]
+        [InlineData("00:00:00.123", "0.123s")]
+        [InlineData("-00:00:00.123", "-0.123s")]
+        [InlineData("00:00:00.123456", "0.123s and 456.0µs")]
+        [InlineData("-00:00:00.123456", "-0.123s and 456.0µs")]
+        [InlineData("00:00:00.1234567", "0.123s and 456.7µs")]
+        [InlineData("-00:00:00.1234567", "-0.123s and 456.7µs")]
+        [InlineData("00:00:04", "4s")]
+        [InlineData("-00:00:04", "-4s")]
+        [InlineData("00:03:04", "3m and 4s")]
+        [InlineData("-00:03:04", "-3m and 4s")]
+        [InlineData("1.02:03:04", "1d, 2h, 3m and 4s")]
+        [InlineData("-1.02:03:04", "-1d, 2h, 3m and 4s")]
+        [InlineData("01:02:03", "1h, 2m and 3s")]
+        [InlineData("-01:02:03", "-1h, 2m and 3s")]
+        [InlineData("01:02:03.123", "1h, 2m and 3.123s")]
+        [InlineData("-01:02:03.123", "-1h, 2m and 3.123s")]
+        [InlineData("01:02:03.123456", "1h, 2m, 3.123s and 456.0µs")]
+        [InlineData("-01:02:03.123456", "-1h, 2m, 3.123s and 456.0µs")]
+        [InlineData("01:02:03.1234567", "1h, 2m, 3.123s and 456.7µs")]
+        [InlineData("-01:02:03.1234567", "-1h, 2m, 3.123s and 456.7µs")]
+        public void When_timespan_components_are_not_relevant_they_should_not_be_included_in_the_output(String actual, String expected)
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var formatter = new TimeSpanValueFormatter();
+            var value = TimeSpan.Parse(actual);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            string result = formatter.Format(value, new FormattingContext(), null);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.Should().Be(expected);
         }
     }
 }
