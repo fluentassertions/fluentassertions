27321e57de3c2f54cadfa3488685ad47a70393b4
Erik Iwarson <erikiwarson@gmail.com>
2018-01-26 06:13:57 +0100

    Update AndConstraint.cs (#742)
    Changed variable "T And" to an auto-implemented property

--- Src/FluentAssertions/AndConstraint.cs
+++ Src/FluentAssertions/AndConstraint.cs
@@ -5,19 +5,14 @@ namespace FluentAssertions
     [DebuggerNonUserCode]
     public class AndConstraint<T>
     {
-        private readonly T parentConstraint;
+        public T And { get; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:System.Object"/> class.
         /// </summary>
         public AndConstraint(T parentConstraint)
         {
-            this.parentConstraint = parentConstraint;
-        }
-
-        public T And
-        {
-            get { return parentConstraint; }
+            And = parentConstraint;
         }
     }
 }
