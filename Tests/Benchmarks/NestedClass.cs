4c81c0740205c974035b68911f4f9aba029512f0
jnyrup <jnyrup@gmail.com>
2018-03-31 08:10:36 +0200

    Add Benchmark project

---
+++ Tests/Benchmarks/NestedClass.cs
@@ -0,0 +1,24 @@
+﻿namespace Benchmarks
+{
+    public sealed class Nested
+    {
+        public int A { get; set; }
+
+        public Nested B { get; set; }
+
+        public static Nested Create(int i)
+        {
+            if (i == 0)
+            {
+                return new Nested();
+            }
+
+            return new Nested
+            {
+                A = i,
+                B = Create(i - 1)
+            };
+        }
+    }
+
+}
---
+++ Tests/Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var summary = BenchmarkRunner.Run<BeEquivalentToBenchmarks>();
+        }
+    }
+}
