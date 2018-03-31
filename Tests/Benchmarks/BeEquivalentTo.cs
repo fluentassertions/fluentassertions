79d35d3a4e7b0daf23a665a2c70f61aa1651f85a
jnyrup <jnyrup@gmail.com>
2018-03-31 08:10:36 +0200

    Add Benchmark project

---
+++ Tests/Benchmarks/BeEquivalentTo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using FluentAssertions;
+using FluentAssertions.Collections;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    [RyuJitX86Job]
+    public class BeEquivalentToBenchmarks
+    {
+        private List<Nested> list;
+        private List<Nested> list2;
+
+        [Params(10, 100, 1_000, 5_000, 10_000)]
+        public int N { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            list = Enumerable.Range(0, N).Select(_ => Nested.Create(1)).ToList();
+            list2 = Enumerable.Range(0, N).Select(_ => Nested.Create(1)).ToList();
+        }
+
+        [Benchmark]
+        public AndConstraint<GenericCollectionAssertions<Nested>> BeEquivalentTo() => list.Should().BeEquivalentTo(list2);
+    }
+}
