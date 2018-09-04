40322e00e9636b30330d3a70d1b08f1298fab1b1
Dong Hyun Kim <wplong11@users.noreply.github.com>
2018-09-04 17:24:58 +0900

    Edit assembly copyright info (#910)

--- Src/SolutionInfo.cs
+++ Src/SolutionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("www.continuousimprover.com")]
-[assembly: AssemblyCopyright("Copyright Dennis Doomen 2010-2016")]
+[assembly: AssemblyCopyright("Copyright Dennis Doomen 2010-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
