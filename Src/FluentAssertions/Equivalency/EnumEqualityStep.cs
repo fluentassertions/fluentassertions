08085dd91d40a92ee041fe85013279fe0585ef70
Artur Krajewski <krajek.dev@gmail.com>
2018-09-24 19:40:40 +0200

    Extract enum handling methods, by-value and by-name

--- Src/FluentAssertions/Equivalency/EnumEqualityStep.cs
+++ Src/FluentAssertions/Equivalency/EnumEqualityStep.cs
@@ -35,45 +35,14 @@ namespace FluentAssertions.Equivalency
         public bool Handle(IEquivalencyValidationContext context, IEquivalencyValidator parent,
             IEquivalencyAssertionOptions config)
         {
-            string EnumDescription(object o, decimal? v)
-            {
-                if(o == null || v == null)
-                {
-                    return "null";
-                }
-
-                if (o?.GetType().GetTypeInfo().IsEnum == true)
-                {
-                    string typePart = o.GetType().Name;
-                    string namePart = Enum.GetName(o.GetType(), o);
-                    string valuePart = v?.ToString(CultureInfo.InvariantCulture) ?? "?";
-                    return $"{typePart}.{namePart}({valuePart})";
-                }
-
-                return v.Value.ToString(CultureInfo.InvariantCulture);
-            }
-
             switch (config.EnumEquivalencyHandling)
             {
                 case EnumEquivalencyHandling.ByValue:
-
-                    decimal? subjectsUnderlyingValue = (context.Subject != null) ? Convert.ToDecimal(context.Subject) : (decimal?)null;
-                    decimal? expectationsUnderlyingValue = (context.Expectation != null) ? Convert.ToDecimal(context.Expectation) : (decimal?)null;
-
-                    string subjectsName = EnumDescription(context.Subject, subjectsUnderlyingValue);
-                    string expectationName = EnumDescription(context.Expectation, expectationsUnderlyingValue);
-
-                    Execute.Assertion
-                        .ForCondition(subjectsUnderlyingValue == expectationsUnderlyingValue)
-                        .FailWith("Expected enum to be {0}{reason}, but found {1}.",
-                            expectationName, subjectsName);
+                    HandleByValue(context);
                     break;
 
                 case EnumEquivalencyHandling.ByName:
-                    string subject = context.Subject.ToString();
-                    string expected = context.Expectation.ToString();
-
-                    subject.Should().Be(expected, context.Because, context.BecauseArgs);
+                    HandleByName(context);
                     break;
 
                 default:
@@ -82,6 +51,47 @@ namespace FluentAssertions.Equivalency
             }
 
             return true;
+        }
+
+        private static void HandleByValue(IEquivalencyValidationContext context)
+        {
+            decimal? subjectsUnderlyingValue = (context.Subject != null) ? Convert.ToDecimal(context.Subject) : (decimal?)null;
+            decimal? expectationsUnderlyingValue =
+                (context.Expectation != null) ? Convert.ToDecimal(context.Expectation) : (decimal?)null;
+
+            string subjectsName = EnumDescription(context.Subject, subjectsUnderlyingValue);
+            string expectationName = EnumDescription(context.Expectation, expectationsUnderlyingValue);
+
+            Execute.Assertion
+                .ForCondition(subjectsUnderlyingValue == expectationsUnderlyingValue)
+                .FailWith("Expected enum to be {0}{reason}, but found {1}.",
+                    expectationName, subjectsName);
+        }
+
+        private static void HandleByName(IEquivalencyValidationContext context)
+        {
+            string subject = context.Subject.ToString();
+            string expected = context.Expectation.ToString();
+
+            subject.Should().Be(expected, context.Because, context.BecauseArgs);
+        }
+
+        private static string EnumDescription(object o, decimal? v)
+        {
+            if (o == null || v == null)
+            {
+                return "null";
+            }
+
+            if (o?.GetType().GetTypeInfo().IsEnum == true)
+            {
+                string typePart = o.GetType().Name;
+                string namePart = Enum.GetName(o.GetType(), o);
+                string valuePart = v?.ToString(CultureInfo.InvariantCulture) ?? "?";
+                return $"{typePart}.{namePart}({valuePart})";
+            }
+
+            return v.Value.ToString(CultureInfo.InvariantCulture);
         }
     }
 }
