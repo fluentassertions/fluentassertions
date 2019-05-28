8c3a1287cb1ca033786549d08e9c2a65f886d75e
mu88 <mu88_developer@outlook.com>
2019-05-28 09:34:43 +0200

    Move method 'WithMessage()' away from dedicated partial and add documentation (#1052)

--- Src/FluentAssertions/AssertionExtensions.cs
+++ Src/FluentAssertions/AssertionExtensions.cs
@@ -718,5 +718,28 @@ namespace FluentAssertions
         {
             return subject is TTo ? (TTo)subject : default(TTo);
         }
+
+        /// <summary>
+        ///   Asserts that the thrown exception has a message that matches <paramref name = "expectedWildcardPattern" />.
+        /// </summary>
+        /// <param name = "expectedWildcardPattern">
+        ///   The wildcard pattern with which the exception message is matched, where * and ? have special meanings.
+        /// </param>
+        /// <param name = "because">
+        ///   A formatted phrase as is supported by <see cref = "string.Format(string,object[])" /> explaining why the assertion
+        ///   is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name = "becauseArgs">
+        ///   Zero or more objects to format using the placeholders in <see cref = "because" />.
+        /// </param>
+        public static async Task<ExceptionAssertions<TException>> WithMessage<TException>(
+            this Task<ExceptionAssertions<TException>> task,
+            string expectedWildcardPattern,
+            string because = "",
+            params object[] becauseArgs)
+            where TException : Exception
+        {
+            return (await task).WithMessage(expectedWildcardPattern, because, becauseArgs);
+        }
     }
 }
