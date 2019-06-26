448e4dfe3d7ebac00ce807455155c920fbc5ed60
conklinb <conklinb@outlook.com>
2019-06-26 15:03:56 -0400

    Update DefaultAssertionStrategy.cs

--- Src/FluentAssertions/Execution/DefaultAssertionStrategy.cs
+++ Src/FluentAssertions/Execution/DefaultAssertionStrategy.cs
@@ -1,19 +1,53 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 using FluentAssertions.Common;
 
 namespace FluentAssertions.Execution
 {
-    internal class DefaultAssertionStrategy : IAssertionStrategy
+#if NET45
+    [Serializable]
+#endif
+    internal class CollectingAssertionStrategy : IAssertionStrategy
     {
+        private readonly List<string> failureMessages = new List<string>();
+
         /// <summary>
         /// Returns the messages for the assertion failures that happened until now.
         /// </summary>
-        public IEnumerable<string> FailureMessages
+        public IEnumerable<string> FailureMessages => failureMessages;
+
+        /// <summary>
+        /// Discards and returns the failure messages that happened up to now.
+        /// </summary>
+        public IEnumerable<string> DiscardFailures()
         {
-            get
+            var discardedFailures = failureMessages.ToArray();
+            failureMessages.Clear();
+            return discardedFailures;
+        }
+
+        /// <summary>
+        /// Will throw a combined exception for any failures have been collected since <see cref="StartCollecting"/> was called.
+        /// </summary>
+        public void ThrowIfAny(IDictionary<string, object> context)
+        {
+            if (failureMessages.Any())
             {
-                return new string[0];
+                var builder = new StringBuilder();
+                builder.AppendLine(string.Join(Environment.NewLine, failureMessages));
+
+                if (context.Any())
+                {
+                    foreach (KeyValuePair<string, object> pair in context)
+                    {
+                        builder.AppendFormat("\nWith {0}:\n{1}", pair.Key, pair.Value);
+                    }
+                }
+
+                Services.ThrowException(builder.ToString());
             }
         }
 
@@ -22,22 +56,7 @@ namespace FluentAssertions.Execution
         /// </summary>
         public void HandleFailure(string message)
         {
-            Services.ThrowException(message);
-        }
-
-        /// <summary>
-        /// Discards and returns the failure messages that happened up to now.
-        /// </summary>
-        public IEnumerable<string> DiscardFailures()
-        {
-            return new string[0];
-        }
-
-        /// <summary>
-        /// Will throw a combined exception for any failures have been collected since <see cref="StartCollecting"/> was called.
-        /// </summary>
-        public void ThrowIfAny(IDictionary<string, object> context)
-        {
+            failureMessages.Add(message);
         }
     }
 }
