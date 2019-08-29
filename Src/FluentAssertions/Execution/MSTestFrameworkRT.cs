fcc9cd6157470d6fb5b87f7da166732b4bf691e0
jnyrup <jnyrup@gmail.com>
2019-08-29 19:19:52 +0200

    Remove leftovers from 4.x

--- Src/FluentAssertions/Execution/MSTestFrameworkRT.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FluentAssertions.Execution
-{
-    internal class MSTestFrameworkRT : LateBoundTestFramework
-    {
-        protected override string AssemblyName => "Microsoft.VisualStudio.TestPlatform.UnitTestFramework";
-
-        protected override string ExceptionFullName => "Microsoft.VisualStudio.TestPlatform.UnitTestFramework.AssertFailedException";
-    }
-}
