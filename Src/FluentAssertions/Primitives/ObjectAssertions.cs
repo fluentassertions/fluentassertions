aff93caf86deba1fcf56ea021daf8bfff6984eee
Antao Almada <antao.almada@farfetch.com>
2019-09-21 16:11:23 +0100

    Fix build

--- Src/FluentAssertions/Primitives/ObjectAssertions.cs
+++ Src/FluentAssertions/Primitives/ObjectAssertions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using FluentAssertions.Common;
 using FluentAssertions.Equivalency;
 using FluentAssertions.Execution;
