324a4bbafdc0738ba52aa488bb1b57104ac645a5
jnyrup <jnyrup@gmail.com>
2020-01-04 19:06:02 +0100

    Add API approval tests

---
+++ Tests/Approval.Tests/ApiApproval.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using ApprovalTests;
+using ApprovalTests.Core;
+using ApprovalTests.Reporters;
+using ApprovalTests.Writers;
+using PublicApiGenerator;
+using Xunit;
+
+namespace Approval.Tests
+{
+    public class ApiApproval
+    {
+        [Theory]
+        [InlineData("FluentAssertions", "net45")]
+        [InlineData("FluentAssertions", "net47")]
+        [InlineData("FluentAssertions", "netstandard1.3")]
+        [InlineData("FluentAssertions", "netstandard1.6")]
+        [InlineData("FluentAssertions", "netstandard2.0")]
+        [InlineData("FluentAssertions", "netstandard2.1")]
+        [InlineData("FluentAssertions", "netcoreapp2.0")]
+        [InlineData("FluentAssertions", "netcoreapp2.1")]
+        [UseReporter(typeof(DiffReporter))]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void ApproveApi(string projectName, string frameworkVersion)
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(new Uri(codeBase));
+            string assemblyPath = Uri.UnescapeDataString(uri.Path);
+            var containingDirectory = Path.GetDirectoryName(assemblyPath);
+            var configurationName = new DirectoryInfo(containingDirectory).Parent.Name;
+            var assemblyFile = Path.GetFullPath(
+                Path.Combine(
+                    GetSourceDirectory(),
+                    $"../../Artifacts/{configurationName}/{frameworkVersion}/{projectName}.dll"));
+
+            var assembly = Assembly.LoadFile(Path.GetFullPath(assemblyFile));
+            var publicApi = ApiGenerator.GeneratePublicApi(assembly, options: null);
+
+            Approvals.Verify(
+                WriterFactory.CreateTextWriter(publicApi),
+                new ApprovalNamer(projectName, frameworkVersion),
+                Approvals.GetReporter());
+        }
+
+        private class ApprovalNamer : IApprovalNamer
+        {
+            public ApprovalNamer(string projectName, string frameworkVersion)
+            {
+                Name = frameworkVersion;
+                SourcePath = Path.Combine(GetSourceDirectory(), "ApprovedApi", projectName);
+            }
+
+            public string SourcePath { get; }
+
+            public string Name { get; }
+        }
+
+        private static string GetSourceDirectory([CallerFilePath] string path = "") => Path.GetDirectoryName(path);
+    }
+}
