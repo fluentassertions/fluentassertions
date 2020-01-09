aace9f7fc4f5195c79161e99ef8a141cc8ebd4f6
Artur Krajewski <krajek.dev@gmail.com>
2020-01-09 10:08:30 +0100

    Add script for easier approval of new API (#1222)

---
+++ AcceptApiChanges.ps1
@@ -0,0 +1,19 @@
+## This script is related to approval tests that protect developers from unintentional changes to the public API
+## If your change does change the API on purpose and you double-checked correctness of the changes you can use this script to change the "approved" state of the API
+## Make sure that you run the approval tests before running this script, because the tests generate *.received.txt files.
+
+
+## Go to the directory where "approved" API is saved as .txt files
+Push-Location -Path .\Tests\Approval.Tests\ApprovedApi\FluentAssertions
+
+## Remove current "approved" API 
+Remove-Item *.approved.txt*
+
+## Copy new API from .received.txt files to .approved.txt files
+## Note that .received.txt files are ignored in git and are not part of the repository
+Get-ChildItem -Filter "*received.txt" | ForEach-Object {
+	$NewName = $_.FullName -replace 'received.txt', 'approved.txt'
+	Copy-Item $_.FullName $NewName
+}
+
+Pop-Location
