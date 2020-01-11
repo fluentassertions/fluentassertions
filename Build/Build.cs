5aab044c73f062c021fa821bb1c42d12b9feac23
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2020-01-11 18:05:55 +0100

    Split the testing targets to simplify local building

--- Build/Build.cs
+++ Build/Build.cs
@@ -1,20 +1,13 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Execution;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Tools.NUnit;
 using Nuke.Common.Tools.Xunit;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using static Nuke.Common.Tools.NUnit.NUnitTasks;
 using static Nuke.Common.Tools.Xunit.XunitTasks;
 
 [CheckBuildProjectConfigurations]
@@ -28,15 +21,17 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
     public static int Main() => Execute<Build>(
-        x => x.Test,
+        x => x.UnitTests,
         x => x.Pack);
 
     [Solution] readonly Solution Solution;
     [GitVersion] readonly GitVersion GitVersion;
+    [PackageExecutable("nspec", "NSpecRunner.exe", Version = "3.1.0")] Tool NSpec3;
 
     AbsolutePath ArtifactsDirectory => RootDirectory / "Artifacts";
     AbsolutePath TestsDirectory => RootDirectory / "Tests";
     AbsolutePath TestFrameworkDirectory => TestsDirectory / "TestFrameworks";
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
@@ -65,15 +60,23 @@ class Build : NukeBuild
                 .SetInformationalVersion(GitVersion.InformationalVersion));
         });
 
-    const string XunitFramework = "net47";
-    [PackageExecutable("nspec", "NSpecRunner.exe", Version = "3.1.0")] Tool NSpec3;
 
-    Target Test => _ => _
+    Target ApiChecks => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetConfiguration(Configuration.Debug)
+                .CombineWith(
+                    cc => cc.SetProjectFile(Solution.GetProject("Approval.Tests"))));
+        });
+
+    Target UnitTests => _ => _
         .DependsOn(Compile)
         .Executes(() =>
         {
             Xunit2(s => s
-                .SetFramework(XunitFramework)
+                .SetFramework("net47")
                 .AddTargetAssemblies(GlobFiles(
                     TestsDirectory,
                     $"Net4*.Specs/bin/Debug/**/*.Specs.dll").NotEmpty()));
@@ -85,6 +88,13 @@ class Build : NukeBuild
                     cc => cc.SetProjectFile(Solution.GetProject("NetCore21.Specs")),
                     cc => cc.SetProjectFile(Solution.GetProject("NetCore30.Specs"))));
 
+
+        });
+
+    Target TestFrameworks => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
             DotNetTest(s => s
                 .SetConfiguration(Configuration.Debug)
                 .CombineWith(
@@ -94,15 +104,12 @@ class Build : NukeBuild
                     cc => cc.SetProjectFile(Solution.GetProject("XUnit2.Specs"))));
 
             NSpec3(TestFrameworkDirectory / "NSpec3.Net47.Specs" / "bin" / "Debug" / "net47" / "NSpec3.Specs.dll");
-
-            DotNetTest(s => s
-                .SetConfiguration(Configuration.Debug)
-                .CombineWith(
-                    cc => cc.SetProjectFile(Solution.GetProject("Approval.Tests"))));
         });
 
     Target Pack => _ => _
-        .DependsOn(Compile)
+        .DependsOn(ApiChecks)
+        .DependsOn(TestFrameworks)
+        .DependsOn(UnitTests)
         .Executes(() =>
         {
             DotNetPack(s => s
