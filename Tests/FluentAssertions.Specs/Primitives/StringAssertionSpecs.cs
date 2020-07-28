cfc4dc52f7fd5781f405e97db85856042a3f3969
Shimmy <2716316+weitzhandler@users.noreply.github.com>
2020-07-28 21:23:04 +0300

    Implemented string upper/lower case assertions (#1357)
    Co-authored-by: Jonas Nyrup <jnyrup@users.noreply.github.com>

--- Tests/FluentAssertions.Specs/Primitives/StringAssertionSpecs.cs
+++ Tests/FluentAssertions.Specs/Primitives/StringAssertionSpecs.cs
@@ -3243,5 +3243,266 @@ namespace FluentAssertions.Specs
         }
 
         #endregion
+
+        #region (Not) Upper
+
+        [Fact]
+        public void When_an_upper_case_string_is_supposed_to_be_in_upper_case_only_it_should_not_throw()
+        {
+            // Arrange
+            string actual = "ABC";
+
+            // Act / Assert
+            actual.Should().BeUpperCased();
+        }
+
+        [Fact]
+        public void When_a_non_upper_string_is_supposed_to_be_upper_it_should_throw()
+        {
+            // Arrange
+            string actual = "abc";
+
+            // Act
+            Action act = () => actual.Should().BeUpperCased();
+
+            // Assert
+            act.Should().Throw<XunitException>();
+        }
+
+        [Fact]
+        public void When_an_upper_case_string_with_numbers_is_supposed_to_be_in_upper_case_only_it_should_throw()
+        {
+            // Arrange
+            string actual = "A1";
+
+            // Act
+            Action act = () => actual.Should().BeUpperCased();
+
+            // Assert
+            act.Should().Throw<XunitException>();
+        }
+
+        [Fact]
+        public void When_a_non_upper_string_is_supposed_to_be_upper_it_should_fail_with_descriptive_message()
+        {
+            // Arrange
+            string actual = "abc";
+
+            // Act
+            Action act = () => actual.Should().BeUpperCased("because we want to test the failure {0}", "message");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                @"Expected all characters in actual to be upper cased because we want to test the failure message, but found ""abc"".");
+        }
+
+        [Fact]
+        public void When_checking_for_an_upper_string_and_it_is_null_it_should_throw()
+        {
+            // Arrange
+            string nullString = null;
+
+            // Act
+            Action act = () => nullString.Should().BeUpperCased("because strings should never be {0}", "null");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Expected all characters in nullString to be upper cased because strings should never be null, but found <null>.");
+        }
+
+        [Fact]
+        public void When_a_non_upper_string_is_supposed_to_be_non_upper_it_should_succeed()
+        {
+            // Arrange
+            string actual = "abc";
+
+            // Act / Assert
+            actual.Should().NotBeUpperCased();
+        }
+
+        [Fact]
+        public void When_a_null_string_is_not_supposed_to_be_upper_it_should_succeed()
+        {
+            // Arrange
+            string actual = null;
+
+            // Act / Assert
+            actual.Should().NotBeUpperCased();
+        }
+
+        [Fact]
+        public void When_an_upper_string_is_not_supposed_to_be_upper_it_should_throw()
+        {
+            // Arrange
+            string actual = "ABC";
+
+            // Act
+            Action act = () => actual.Should().NotBeUpperCased();
+
+            // Assert
+            act.Should().Throw<XunitException>();
+        }
+
+        [Fact]
+        public void When_an_upper_case_string_with_numbers_is_not_supposed_to_be_in_upper_case_only_it_should_succeed()
+        {
+            // Arrange
+            string actual = "a1";
+
+            // Act / Assert
+            Action act = () => actual.Should().NotBeUpperCased();
+        }
+
+        [Fact]
+        public void When_an_upper_string_is_not_supposed_to_be_upper_it_should_fail_with_descriptive_message()
+        {
+            // Arrange
+            string actual = "ABC";
+
+            // Act
+            Action act = () => actual.Should().NotBeUpperCased("because we want to test the failure {0}", "message");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Did not expect any characters in actual to be upper cased because we want to test the failure message.");
+        }
+
+        #endregion (Not) Upper
+
+        #region (Not) Lower
+
+        [Fact]
+        public void When_a_lower_string_is_supposed_to_be_lower_it_should_succeed()
+        {
+            // Arrange
+            string actual = "abc";
+
+            // Act / Assert
+            actual.Should().BeLowerCased();
+        }
+
+        [Fact]
+        public void When_an_empty_string_is_supposed_to_be_lower_it_should_succeed()
+        {
+            // Arrange
+            string actual = "";
+
+            // Act / Assert
+            actual.Should().BeLowerCased();
+        }
+
+        [Fact]
+        public void When_a_non_lower_string_is_supposed_to_be_lower_it_should_fail()
+        {
+            // Arrange
+            string actual = "ABC";
+
+            // Act
+            Action act = () => actual.Should().BeLowerCased();
+
+            // Assert
+            act.Should().Throw<XunitException>();
+        }
+
+        [Fact]
+        public void When_a_lower_case_string_with_numbers_is_supposed_to_be_in_lower_case_only_it_should_throw()
+        {
+            // Arrange
+            string actual = "a1";
+
+            // Act
+            Action act = () => actual.Should().BeLowerCased();
+
+            // Assert
+            act.Should().Throw<XunitException>();
+        }
+
+        [Fact]
+        public void When_a_non_lower_string_is_supposed_to_be_lower_it_should_fail_with_descriptive_message()
+        {
+            // Arrange
+            string actual = "ABC";
+
+            // Act
+            Action act = () => actual.Should().BeLowerCased("because we want to test the failure {0}", "message");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Expected all characters in actual to be lower cased because we want to test the failure message, but found \"ABC\".");
+        }
+
+        [Fact]
+        public void When_checking_for_a_lower_string_and_it_is_null_it_should_throw()
+        {
+            // Arrange
+            string nullString = null;
+
+            // Act
+            Action act = () => nullString.Should().BeLowerCased("because strings should never be {0}", "null");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Expected all characters in nullString to be lower cased because strings should never be null, but found <null>.");
+        }
+
+        [Fact]
+        public void When_a_non_lower_string_is_supposed_to_be_upper_it_should_succeed()
+        {
+            // Arrange
+            string actual = "ABC";
+
+            // Act / Assert
+            actual.Should().NotBeLowerCased();
+        }
+
+        [Fact]
+        public void When_a_null_string_is_not_supposed_to_be_lower_it_should_succeed()
+        {
+            // Arrange
+            string actual = null;
+
+            // Act / Assert
+            actual.Should().NotBeLowerCased();
+        }
+
+        [Fact]
+        public void When_a_lower_string_is_supposed_to_be_upper_it_should_throw()
+        {
+            // Arrange
+            string actual = "abc";
+
+            // Act
+            Action act = () => actual.Should().NotBeLowerCased();
+
+            // Assert
+            act.Should().Throw<XunitException>();
+        }
+
+        [Fact]
+        public void When_a_lower_case_string_with_numbers_is_not_supposed_to_be_in_lower_case_only_it_should_succeed()
+        {
+            // Arrange
+            string actual = "A1";
+
+            // Act / Assert
+            Action act = () => actual.Should().NotBeLowerCased();
+        }
+
+        [Fact]
+        public void When_a_lower_string_is_not_supposed_to_be_lower_it_should_fail_with_descriptive_message()
+        {
+            // Arrange
+            string actual = "abc";
+
+            // Act
+            Action act = () => actual.Should().NotBeLowerCased("because we want to test the failure {0}", "message");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Did not expect any characters in actual to be lower cased because we want to test the failure message.");
+        }
+
+        #endregion (Not) Lower
+
     }
 }
