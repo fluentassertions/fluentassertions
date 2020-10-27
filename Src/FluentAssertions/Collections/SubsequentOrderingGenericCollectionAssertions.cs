ce48329141212c77190d80ffdd69cfbd7d399835
Serg046 <serg046@outlook.com>
2020-10-27 23:55:24 +0300

    Add a release note and contract docs

--- Src/FluentAssertions/Collections/SubsequentOrderingGenericCollectionAssertions.cs
+++ Src/FluentAssertions/Collections/SubsequentOrderingGenericCollectionAssertions.cs
@@ -42,24 +42,98 @@ namespace FluentAssertions.Collections
             this.previousOrderedEnumerable = previousOrderedEnumerable;
         }
 
+        /// <summary>
+        /// Asserts that a subsequence is ordered in ascending order according to the value of the specified
+        /// <paramref name="propertyExpression"/>.
+        /// </summary>
+        /// <param name="propertyExpression">
+        /// A lambda expression that references the property that should be used to determine the expected ordering.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])"/> explaining why the assertion
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <paramref name="because" />.
+        /// </param>
+        /// <remarks>
+        /// Empty and single element collections are considered to be ordered both in ascending and descending order at the same time.
+        /// </remarks>
         public AndConstraint<SubsequentOrderingGenericCollectionAssertions<T>> ThenBeInAscendingOrder<TSelector>(
             Expression<Func<T, TSelector>> propertyExpression, string because = "", params object[] becauseArgs)
         {
             return ThenBeInAscendingOrder(propertyExpression, Comparer<TSelector>.Default, because, becauseArgs);
         }
 
+        /// <summary>
+        /// Asserts that a subsequence is ordered in ascending order according to the value of the specified
+        /// <paramref name="propertyExpression"/> and <see cref="IComparer{T}"/> implementation.
+        /// </summary>
+        /// <param name="propertyExpression">
+        /// A lambda expression that references the property that should be used to determine the expected ordering.
+        /// </param>
+        /// <param name="comparer">
+        /// The object that should be used to determine the expected ordering.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])"/> explaining why the assertion
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <paramref name="because" />.
+        /// </param>
+        /// <remarks>
+        /// Empty and single element collections are considered to be ordered both in ascending and descending order at the same time.
+        /// </remarks>
         public AndConstraint<SubsequentOrderingGenericCollectionAssertions<T>> ThenBeInAscendingOrder<TSelector>(
             Expression<Func<T, TSelector>> propertyExpression, IComparer<TSelector> comparer, string because = "", params object[] becauseArgs)
         {
             return ThenBeOrderedBy(propertyExpression, comparer, SortOrder.Ascending, because, becauseArgs);
         }
 
+        /// <summary>
+        /// Asserts that a subsequence is ordered in descending order according to the value of the specified
+        /// <paramref name="propertyExpression"/>.
+        /// </summary>
+        /// <param name="propertyExpression">
+        /// A lambda expression that references the property that should be used to determine the expected ordering.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])"/> explaining why the assertion
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <paramref name="because" />.
+        /// </param>
+        /// <remarks>
+        /// Empty and single element collections are considered to be ordered both in ascending and descending order at the same time.
+        /// </remarks>
         public AndConstraint<SubsequentOrderingGenericCollectionAssertions<T>> ThenBeInDescendingOrder<TSelector>(
             Expression<Func<T, TSelector>> propertyExpression, string because = "", params object[] becauseArgs)
         {
             return ThenBeInDescendingOrder(propertyExpression, Comparer<TSelector>.Default, because, becauseArgs);
         }
 
+        /// <summary>
+        /// Asserts that a subsequence is ordered in descending order according to the value of the specified
+        /// <paramref name="propertyExpression"/> and <see cref="IComparer{T}"/> implementation.
+        /// </summary>
+        /// <param name="propertyExpression">
+        /// A lambda expression that references the property that should be used to determine the expected ordering.
+        /// </param>
+        /// <param name="comparer">
+        /// The object that should be used to determine the expected ordering.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])"/> explaining why the assertion
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <paramref name="because" />.
+        /// </param>
+        /// <remarks>
+        /// Empty and single element collections are considered to be ordered both in ascending and descending order at the same time.
+        /// </remarks>
         public AndConstraint<SubsequentOrderingGenericCollectionAssertions<T>> ThenBeInDescendingOrder<TSelector>(
             Expression<Func<T, TSelector>> propertyExpression, IComparer<TSelector> comparer, string because = "", params object[] becauseArgs)
         {
