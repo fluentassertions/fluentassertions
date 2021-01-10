21e572bef5860d1d31fb28515409e37b693ccf70
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2021-01-10 13:51:27 +0100

    Update _build.csproj

--- Build/Build.cs
+++ Build/Build.cs
@@ -28,7 +28,8 @@ class Build : NukeBuild
         x => x.Pack);
 
     [Solution] readonly Solution Solution;
-    [GitVersion] readonly GitVersion GitVersion;
+    [GitVersion(Framework = "net5.0", NoFetch = true)]
+    readonly GitVersion GitVersion;
     [PackageExecutable("nspec", "NSpecRunner.exe", Version = "3.1.0")] Tool NSpec3;
 
     AbsolutePath ArtifactsDirectory => RootDirectory / "Artifacts";
