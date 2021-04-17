4d85e3a7d9435ede04cd19273d66d683e54aa1e6
Jonas Nyrup <jnyrup@gmail.com>
2021-04-17 16:49:28 +0200

    Scan AppDomain to detect nspec assembly

--- Src/FluentAssertions/Execution/XUnit2TestFramework.cs
+++ Src/FluentAssertions/Execution/XUnit2TestFramework.cs
@@ -13,6 +13,7 @@ namespace FluentAssertions.Execution
             {
                 try
                 {
+                    // For netfx the assembly is not in AppDomain by default, so we can't just scan AppDomain.CurrentDomain
                     assembly = Assembly.Load(new AssemblyName("xunit.assert"));
 
                     return assembly is not null;
