46aed0245bdec00a689755774b710d516a25f572
Jonas Nyrup <jnyrup@gmail.com>
2021-09-05 19:18:01 +0200

    Fix compiler warnings

--- Tests/FluentAssertions.Specs/IsExternalInit.cs
+++ Tests/FluentAssertions.Specs/IsExternalInit.cs
@@ -1,8 +1,8 @@
-﻿// ReSharper disable once CheckNamespace
+﻿using System.ComponentModel;
+
+// ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
 {
-    using System.ComponentModel;
-
     /// <summary>
     /// Reserved to be used by the compiler for tracking metadata.
     /// This class should not be used by developers in source code.
