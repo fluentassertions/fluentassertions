58dca7ef7fde63b084c4e1c3fbb3826a821d7981
Michaël Hompus <michael@hompus.nl>
2022-03-08 22:10:53 +0100

    Split exception specs in multiple files

---
+++ Tests/FluentAssertions.Specs/Exceptions/InvokingActionSpecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace FluentAssertions.Specs.Exceptions
+{
+    public class InvokingActionSpecs
+    {
+        [Fact]
+        public void When_invoking_an_action_on_a_null_subject_it_should_throw()
+        {
+            // Arrange
+            Does someClass = null;
+
+            // Act
+            Action act = () => someClass.Invoking(d => d.Do());
+
+            // Assert
+            act.Should().ThrowExactly<ArgumentNullException>()
+                .WithParameterName("subject");
+        }
+
+        [Fact]
+        public void When_invoking_an_action_with_null_it_should_throw()
+        {
+            // Arrange
+            Does someClass = Does.NotThrow();
+
+            // Act
+            Action act = () => someClass.Invoking(null).Should().NotThrow();
+
+            // Assert
+            act.Should().ThrowExactly<ArgumentNullException>()
+                .WithParameterName("action");
+        }
+    }
+}
---
+++ Tests/FluentAssertions.Specs/Exceptions/InvokingFunctionSpecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace FluentAssertions.Specs.Exceptions
+{
+    public class InvokingFunctionSpecs
+    {
+        [Fact]
+        public void When_invoking_a_function_on_a_null_subject_it_should_throw()
+        {
+            // Arrange
+            Does someClass = null;
+
+            // Act
+            Action act = () => someClass.Invoking(d => d.ToString());
+
+            // Assert
+            act.Should().ThrowExactly<ArgumentNullException>()
+                .WithParameterName("subject");
+        }
+
+        [Fact]
+        public void When_invoking_a_function_with_null_it_should_throw()
+        {
+            // Arrange
+            object someClass = new();
+
+            // Act
+            Action act = () => someClass.Invoking((Func<object, string>)null);
+
+            // Assert
+            act.Should().ThrowExactly<ArgumentNullException>()
+                .WithParameterName("action");
+        }
+    }
+}
