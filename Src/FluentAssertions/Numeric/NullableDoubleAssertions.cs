a0f8165b1a4a10328dbc3f5dd96cb0ff88d2f5dd
Jonas Nyrup <jnyrup@gmail.com>
2022-03-27 21:57:59 +0200

    Better handling of NaN in nullable numeric assertions

---
+++ Src/FluentAssertions/Numeric/NullableDoubleAssertions.cs
@@ -0,0 +1,12 @@
+﻿namespace FluentAssertions.Numeric
+{
+    internal class NullableDoubleAssertions : NullableNumericAssertions<double>
+    {
+        public NullableDoubleAssertions(double? value)
+            : base(value)
+        {
+        }
+
+        private protected override bool IsNaN(double value) => double.IsNaN(value);
+    }
+}
---
+++ Src/FluentAssertions/Numeric/NullableFloatAssertions.cs
@@ -0,0 +1,12 @@
+﻿namespace FluentAssertions.Numeric
+{
+    internal class NullableFloatAssertions : NullableNumericAssertions<float>
+    {
+        public NullableFloatAssertions(float? value)
+            : base(value)
+        {
+        }
+
+        private protected override bool IsNaN(float value) => float.IsNaN(value);
+    }
+}
