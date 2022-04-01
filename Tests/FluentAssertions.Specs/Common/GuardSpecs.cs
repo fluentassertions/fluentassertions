b571510f99f6c0931d414b5c551b04dd20ce5782
Lukas Gasselsberger | alu-one <l.gasselsberger@alu-one.at>
2022-04-01 08:18:52 +0200

    Add tests for missing `Guard` methods

--- Tests/FluentAssertions.Specs/Common/GuardSpecs.cs
+++ Tests/FluentAssertions.Specs/Common/GuardSpecs.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions.Common;
+using FluentAssertions.Execution;
 using Xunit;
 
 namespace FluentAssertions.Specs.Common
@@ -11,7 +12,7 @@ namespace FluentAssertions.Specs.Common
     public class GuardSpecs
     {
         [Fact]
-        public void When_element_is_null_it_throws()
+        public void When_element_is_null_it_throws_with_empty_message()
         {
             // Arrange
             object o = null;
@@ -24,7 +25,20 @@ namespace FluentAssertions.Specs.Common
         }
 
         [Fact]
-        public void When_element_is_not_null_it_do_not_throw()
+        public void When_element_is_null_it_throws_with_no_message()
+        {
+            // Arrange
+            object o = null;
+
+            // Act
+            Action act = () => Guard.ThrowIfArgumentIsNull(o, nameof(o));
+
+            // Assert
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void When_element_is_not_null_it_do_not_throw_with_empty_message()
         {
             // Arrange
             object o = new object();
@@ -36,10 +50,35 @@ namespace FluentAssertions.Specs.Common
             act.Should().NotThrow();
         }
 
+        [Fact]
+        public void When_element_is_not_null_it_do_not_throw_with_no_message()
+        {
+            // Arrange
+            object o = new object();
+
+            // Act
+            Action act = () => Guard.ThrowIfArgumentIsNull(o, nameof(o));
+
+            // Assert
+            act.Should().NotThrow();
+        }
+
         [Theory]
         [InlineData("")]
         [InlineData(null)]
-        public void When_argument_is_null_or_empty_it_throws(string s)
+        public void When_argument_is_null_or_empty_it_throws_with_empty_message(string s)
+        {
+            // Act
+            Action act = () => Guard.ThrowIfArgumentIsNullOrEmpty(s, nameof(s), "");
+
+            // Assert
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void When_argument_is_null_or_empty_it_throws_with_no_message(string s)
         {
             // Act
             Action act = () => Guard.ThrowIfArgumentIsNullOrEmpty(s, nameof(s));
@@ -51,7 +90,19 @@ namespace FluentAssertions.Specs.Common
         [Theory]
         [InlineData("a")]
         [InlineData("\n")]
-        public void When_argument_is_not_null_or_empty_it_does_not_throw(string s)
+        public void When_argument_is_not_null_or_empty_it_does_not_throw_with_empty_message(string s)
+        {
+            // Act
+            Action act = () => Guard.ThrowIfArgumentIsNullOrEmpty(s, nameof(s), "");
+
+            // Assert
+            act.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData("a")]
+        [InlineData("\n")]
+        public void When_argument_is_not_null_or_empty_it_does_not_throw_with_no_message(string s)
         {
             // Act
             Action act = () => Guard.ThrowIfArgumentIsNullOrEmpty(s, nameof(s));
@@ -61,7 +112,20 @@ namespace FluentAssertions.Specs.Common
         }
 
         [Fact]
-        public void When_any_of_the_elements_is_null_it_throws()
+        public void When_any_of_the_elements_is_null_it_throws_with_empty_message()
+        {
+            // Arrange
+            object[] o = new object[] { new object(), null };
+
+            // Act
+            Action act = () => Guard.ThrowIfArgumentContainsNull(o, nameof(o), "");
+
+            // Assert
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void When_any_of_the_elements_is_null_it_throws_with_no_message()
         {
             // Arrange
             object[] o = new object[] { new object(), null };
@@ -74,7 +138,20 @@ namespace FluentAssertions.Specs.Common
         }
 
         [Fact]
-        public void When_all_of_the_elements_are_not_null_it_does_not_throw()
+        public void When_all_of_the_elements_are_not_null_it_does_not_throw_with_empty_message()
+        {
+            // Arrange
+            object[] o = new object[] { new object(), new object() };
+
+            // Act
+            Action act = () => Guard.ThrowIfArgumentContainsNull(o, nameof(o), "");
+
+            // Assert
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void When_all_of_the_elements_are_not_null_it_does_not_throw_with_no_message()
         {
             // Arrange
             object[] o = new object[] { new object(), new object() };
