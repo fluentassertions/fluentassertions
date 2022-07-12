b27873f68dc5d4edeca458d8d62612891280e2f6
Andreas Pazureck <andreas.pazureck.ext@zeiss.com>
2022-07-12 10:49:51 +0200

    fixed build errors due to removing generic from options

--- Src/FluentAssertions/Events/EventMonitor.cs
+++ Src/FluentAssertions/Events/EventMonitor.cs
@@ -18,11 +18,11 @@ internal sealed class EventMonitor<T> : IMonitor<T>
 
     private readonly ConcurrentDictionary<string, EventRecorder> recorderMap = new();
 
-    public EventMonitor(object eventSource, EventMonitorOptions<T> options)
+    public EventMonitor(object eventSource, EventMonitorOptions options)
     {
         Guard.ThrowIfArgumentIsNull(eventSource, nameof(eventSource), "Cannot monitor the events of a <null> object.");
 
-        this.options = options ?? new EventMonitorOptions<T>();
+        this.options = options ?? new EventMonitorOptions();
 
         subject = new WeakReference(eventSource);
 
@@ -32,7 +32,7 @@ internal sealed class EventMonitor<T> : IMonitor<T>
     public T Subject => (T)subject.Target;
 
     private readonly ThreadSafeSequenceGenerator threadSafeSequenceGenerator = new();
-    private readonly EventMonitorOptions<T> options;
+    private readonly EventMonitorOptions options;
 
     public EventMetadata[] MonitoredEvents
     {
