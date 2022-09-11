00d99cc738d82fea96dd723161448d2fd219539d
Jonas Nyrup <jnyrup@gmail.com>
2022-09-11 13:50:34 +0200

    Remove unused using

--- Src/FluentAssertions/AssertionExtensions.cs
+++ Src/FluentAssertions/AssertionExtensions.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Net.Http;
 using System.Reflection;
