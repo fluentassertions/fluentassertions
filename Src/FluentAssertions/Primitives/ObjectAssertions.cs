3a97616da682e08673b2ecbab8c96b3a01866796
Steve Ognibene <steve.ognibene@outlook.com>
2022-09-30 22:15:12 -0400

    fix pragma warning disables/don't override GetHashCode()

--- Src/FluentAssertions/Primitives/ObjectAssertions.cs
+++ Src/FluentAssertions/Primitives/ObjectAssertions.cs
@@ -17,6 +17,8 @@ public class ObjectAssertions : ObjectAssertions<object, ObjectAssertions>
     }
 }
 
+#pragma warning disable CS0659 // Ignore not overriding Object.GetHashCode()
+#pragma warning disable CA1065 // Ignore throwing NotSupportedException from Equals
 /// <summary>
 /// Contains a number of methods to assert that a <typeparamref name="TSubject"/> is in the expected state.
 /// </summary>
@@ -220,14 +222,9 @@ public class ObjectAssertions<TSubject, TAssertions> : ReferenceTypeAssertions<T
         return new AndConstraint<TAssertions>((TAssertions)this);
     }
 
-#pragma warning disable CA1065 // "Exceptions should not be raised in this type of method..."; this is intentional to ensure that FluentAssertions' users get a suggestion of what to use instead.
     /// <inheritdoc/>
     public override bool Equals(object obj) =>
         throw new NotSupportedException("Equals is not part of Fluent Assertions. Did you mean Be() or BeSameAs() instead?");
-#pragma warning restore CA1065
-
-    /// <inheritdoc/>
-    public override int GetHashCode() => base.GetHashCode();
 
     /// <summary>
     /// Returns the type of the subject the assertion applies on.
