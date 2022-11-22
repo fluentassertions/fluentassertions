14891ccce567946f193b8e5658b30c92394da880
Lukas Grützmacher <github@lg2.de>
2022-11-22 15:50:15 +0100

    Add tests for FallbackTestFramework and AssertionFailedException

---
+++ Tests/FluentAssertions.Specs/Execution/FallbackTestFrameworkTests.cs
@@ -0,0 +1,24 @@
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace FluentAssertions.Specs.Execution;
+
+public class FallbackTestFrameworkTests
+{
+    [Fact]
+    public void The_fallback_test_framework_is_available()
+    {
+        var sut = new FallbackTestFramework();
+
+        sut.IsAvailable.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Throwing_with_messages_throws_the_exception()
+    {
+        var sut = new FallbackTestFramework();
+
+        sut.Invoking(x => x.Throw("test message")).Should().ThrowExactly<AssertionFailedException>()
+            .WithMessage("test message");
+    }
+}
