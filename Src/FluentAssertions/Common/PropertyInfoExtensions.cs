5bb0c5bfa74b4277f0dccb8950cdf3ba79ed97b8
94sedighi <kh.baset.sedighi@gmail.com>
2022-12-05 08:25:01 +0100

    Removed some unnecessary documentations

--- Src/FluentAssertions/Common/PropertyInfoExtensions.cs
+++ Src/FluentAssertions/Common/PropertyInfoExtensions.cs
@@ -4,33 +4,18 @@ namespace FluentAssertions.Common;
 
 internal static class PropertyInfoExtensions
 {
-    /// <summary>
-    /// Checks if the property is virtual
-    /// </summary>
-    /// <param name="property"></param>
-    /// <returns></returns>
     internal static bool IsVirtual(this PropertyInfo property)
     {
         MethodInfo methodInfo = property.GetGetMethod(nonPublic: true) ?? property.GetSetMethod(nonPublic: true);
         return !methodInfo.IsNonVirtual();
     }
 
-    /// <summary>
-    /// Checks if the property is static 
-    /// </summary>
-    /// <param name="property"></param>
-    /// <returns></returns>
     internal static bool IsStatic(this PropertyInfo property)
     {
         MethodInfo methodInfo = property.GetGetMethod(nonPublic: true);
         return methodInfo.IsStatic;
     }
 
-    /// <summary>
-    /// Checks if the property is Abstract
-    /// </summary>
-    /// <param name="property"></param>
-    /// <returns></returns>
     internal static bool IsAbstract(this PropertyInfo property)
     {
         MethodInfo methodInfo = property.GetGetMethod(nonPublic: true);
