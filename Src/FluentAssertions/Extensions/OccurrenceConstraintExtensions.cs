7b7948323294963aab373474bc461801a6ebb6ce
Lukas Gasselsberger <it@voecklabrucker-freikirche.at>
2023-01-01 13:00:12 +0100

    Add xml summaries

--- Src/FluentAssertions/Extensions/OccurrenceConstraintExtensions.cs
+++ Src/FluentAssertions/Extensions/OccurrenceConstraintExtensions.cs
@@ -1,17 +1,35 @@
+using System;
+
 namespace FluentAssertions.Extensions;
 
+
+/// <summary>
+/// Provides little extensions to write less bumpy <see cref="OccurrenceConstraint" />s 
+/// </summary>
 public static class OccurrenceConstraintExtensions
 {
+    /// <summary>
+    /// This is the equivalent to <see cref="Exactly.Times(int)" />
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="times"/> is less than zero.</exception>
     public static OccurrenceConstraint TimesExactly(this int times)
     {
         return Exactly.Times(times);
     }
 
+    /// <summary>
+    /// This is the equivalent to <see cref="AtMost.Times(int)" />
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="times"/> is less than zero.</exception>
     public static OccurrenceConstraint TimesOrLess(this int times)
     {
         return AtMost.Times(times);
     }
 
+    /// <summary>
+    /// This is the equivalent to <see cref="AtLeast.Times(int)" />
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="times"/> is less than zero.</exception>
     public static OccurrenceConstraint TimesOrMore(this int times)
     {
         return AtLeast.Times(times);
