89348f7c6b585d0280f0b5869133d93c7a94df3a
IT-VBFK <49762557+IT-VBFK@users.noreply.github.com>
2023-01-14 13:04:06 +0100

    Clean up `Build.cs` (#2093)

---
+++ Build/Configuration.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel;
+using Nuke.Common.Tooling;
+
+[TypeConverter(typeof(TypeConverter<Configuration>))]
+public class Configuration : Enumeration
+{
+    public static Configuration Debug = new Configuration { Value = nameof(Debug) };
+    public static Configuration Release = new Configuration { Value = nameof(Release) };
+    public static Configuration CI = new Configuration { Value = nameof(CI) };
+
+    public static implicit operator string(Configuration configuration)
+    {
+        return configuration.Value;
+    }
+}
