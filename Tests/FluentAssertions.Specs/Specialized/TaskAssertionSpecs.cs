612342ce5304759ae9bba3313940744070155549
Lukas Grützmacher <github@lg2.de>
2023-02-19 12:43:11 +0100

    Introduce ThrowWithinAsync

--- Tests/FluentAssertions.Specs/Specialized/TaskAssertionSpecs.cs
+++ Tests/FluentAssertions.Specs/Specialized/TaskAssertionSpecs.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using FluentAssertions.Execution;
 using FluentAssertions.Extensions;
 using Xunit;
 using Xunit.Sdk;
+
+using static FluentAssertions.FluentActions;
 
 namespace FluentAssertions.Specs.Specialized;
 
@@ -44,7 +47,7 @@ public static class TaskAssertionSpecs
         }
 
         [Fact]
-        public async Task When_task_completes_fast_t_should_succeed()
+        public async Task When_task_completes_fast_it_should_succeed()
         {
             // Arrange
             var timer = new FakeClock();
@@ -96,7 +99,6 @@ public static class TaskAssertionSpecs
             // Act
             Func<Task> action = () =>
                 taskFactory.Awaiting(t => (Task)t.Task).Should(timer).CompleteWithinAsync(100.Milliseconds());
-
             timer.Complete();
 
             // Assert
@@ -182,6 +184,285 @@ public static class TaskAssertionSpecs
 
             // Assert
             await action.Should().NotThrowAsync();
+        }
+    }
+
+    public class ThrowAsync
+    {
+        [Fact]
+        public async Task When_subject_is_null_it_should_throw()
+        {
+            // Arrange
+            Func<Task> action = null;
+
+            // Act
+            Func<Task> testAction = () => action.Should().ThrowAsync<InvalidOperationException>(
+                "because we want to test the failure {0}", "message");
+
+            // Assert
+            await testAction.Should().ThrowAsync<XunitException>()
+                .WithMessage("*because we want to test the failure message*found <null>*");
+        }
+
+        [Fact]
+        public async Task When_subject_is_null_in_assertion_scope_it_should_throw()
+        {
+            // Arrange
+            Func<Task> action = null;
+
+            // Act
+            Func<Task> testAction = async () =>
+            {
+                using var _ = new AssertionScope();
+                await action.Should().ThrowAsync<InvalidOperationException>(
+                    "because we want to test the failure {0}", "message");
+            };
+
+            // Assert
+            await testAction.Should().ThrowAsync<XunitException>()
+                .WithMessage("*because we want to test the failure message*found <null>*");
+        }
+
+        [Fact]
+        public async Task When_task_throws_it_should_succeed()
+        {
+            // Act
+            Func<Task> action = () =>
+            {
+                return
+                    Awaiting(() => Task.FromException(new InvalidOperationException("foo")))
+                    .Should().ThrowAsync<InvalidOperationException>();
+            };
+
+            // Assert
+            await action.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async Task When_task_throws_unexpected_exception_it_should_fail()
+        {
+            // Act
+            Func<Task> action = () =>
+            {
+                return
+                    Awaiting(() => Task.FromException(new NotSupportedException("foo")))
+                    .Should().ThrowAsync<InvalidOperationException>();
+            };
+
+            // Assert
+            await action.Should().ThrowAsync<XunitException>().WithMessage(
+                "Expected a <System.InvalidOperationException> to be thrown,"
+                + " but found <System.NotSupportedException>:*with message \"foo\"*");
+        }
+
+        [Fact]
+        public async Task When_task_completes_without_exception_it_should_fail()
+        {
+            // Act
+            Func<Task> action = () =>
+            {
+                return
+                    Awaiting(() => Task.CompletedTask)
+                    .Should().ThrowAsync<InvalidOperationException>();
+            };
+
+            // Assert
+            await action.Should().ThrowAsync<XunitException>().WithMessage(
+                "Expected a <System.InvalidOperationException> to be thrown, but no exception was thrown.");
+        }
+    }
+
+    public class ThrowWithinAsync
+    {
+        [Fact]
+        public async Task When_subject_is_null_it_should_throw()
+        {
+            // Arrange
+            Func<Task> action = null;
+
+            // Act
+            Func<Task> testAction = () => action.Should().ThrowWithinAsync<InvalidOperationException>(
+                100.Milliseconds(), "because we want to test the failure {0}", "message");
+
+            // Assert
+            await testAction.Should().ThrowAsync<XunitException>()
+                .WithMessage("*because we want to test the failure message*found <null>*");
+        }
+
+        [Fact]
+        public async Task When_subject_is_null_in_assertion_scope_it_should_throw()
+        {
+            // Arrange
+            Func<Task> action = null;
+
+            // Act
+            Func<Task> testAction = async () =>
+            {
+                using var _ = new AssertionScope();
+                await action.Should().ThrowWithinAsync<InvalidOperationException>(
+                    100.Milliseconds(), "because we want to test the failure {0}", "message");
+            };
+
+            // Assert
+            await testAction.Should().ThrowAsync<XunitException>()
+                .WithMessage("*because we want to test the failure message*found <null>*");
+        }
+
+        [Theory]
+        [InlineData(99)]
+        [InlineData(100)]
+        public async Task When_task_throws_fast_it_should_succeed(int delay)
+        {
+            // Arrange
+            var timer = new FakeClock();
+            var taskFactory = new TaskCompletionSource<bool>();
+
+            // Act
+            Func<Task> action = () =>
+            {
+                return taskFactory
+                    .Awaiting(t => (Task)t.Task)
+                    .Should(timer).ThrowWithinAsync<InvalidOperationException>(100.Ticks());
+            };
+
+            timer.Delay(delay.Ticks());
+            taskFactory.SetException(new InvalidOperationException("foo"));
+
+            // Assert
+            await action.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async Task When_task_throws_slow_it_should_fail()
+        {
+            // Arrange
+            var timer = new FakeClock();
+            var taskFactory = new TaskCompletionSource<bool>();
+
+            // Act
+            Func<Task> action = () =>
+            {
+                return taskFactory
+                    .Awaiting(t => (Task)t.Task)
+                    .Should(timer).ThrowWithinAsync<InvalidOperationException>(100.Ticks());
+            };
+
+            timer.Delay(101.Ticks());
+            taskFactory.SetException(new InvalidOperationException("foo"));
+            timer.Complete();
+
+            // Assert
+            await action.Should().ThrowAsync<XunitException>();
+        }
+
+        [Fact]
+        public async Task When_task_throws_asynchronous_it_should_succeed()
+        {
+            // Arrange
+            var timer = new FakeClock();
+            var taskFactory = new TaskCompletionSource<bool>();
+
+            // Act
+            Func<Task> action = () =>
+            {
+                return Awaiting(() => (Task)taskFactory.Task)
+                    .Should(timer).ThrowWithinAsync<InvalidOperationException>(1.Seconds());
+            };
+            _ = action.Invoke();
+            taskFactory.SetException(new InvalidOperationException("foo"));
+
+            // Assert
+            await action.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async Task When_task_not_completes_it_should_fail()
+        {
+            // Arrange
+            var timer = new FakeClock();
+            var taskFactory = new TaskCompletionSource<bool>();
+
+            // Act
+            Func<Task> action = () =>
+            {
+                return taskFactory
+                    .Awaiting(t => (Task)t.Task)
+                    .Should(timer).ThrowWithinAsync<InvalidOperationException>(
+                        100.Ticks(), "because we want to test the failure {0}", "message");
+            };
+            timer.Delay(101.Ticks());
+
+            // Assert
+            await action.Should().ThrowAsync<XunitException>().WithMessage(
+                "Expected a <System.InvalidOperationException> to be thrown within 10.0µs"
+                + " because we want to test the failure message,"
+                + " but no exception was thrown.");
+        }
+
+        [Fact]
+        public async Task When_task_completes_without_exception_it_should_fail()
+        {
+            // Arrange
+            var timer = new FakeClock();
+            var taskFactory = new TaskCompletionSource<bool>();
+
+            // Act
+            Func<Task> action = () =>
+            {
+                return taskFactory
+                    .Awaiting(t => (Task)t.Task)
+                    .Should(timer).ThrowWithinAsync<InvalidOperationException>(100.Milliseconds());
+            };
+            taskFactory.SetResult(true);
+            timer.Complete();
+
+            // Assert
+            await action.Should().ThrowAsync<XunitException>().WithMessage(
+                "Expected a <System.InvalidOperationException> to be thrown within 100ms, but no exception was thrown.");
+        }
+
+        [Fact]
+        public async Task When_task_throws_unexpected_exception_it_should_fail()
+        {
+            // Arrange
+            var timer = new FakeClock();
+            var taskFactory = new TaskCompletionSource<bool>();
+
+            // Act
+            Func<Task> action = () =>
+            {
+                return taskFactory
+                    .Awaiting(t => (Task)t.Task)
+                    .Should(timer).ThrowWithinAsync<InvalidOperationException>(100.Milliseconds());
+            };
+            taskFactory.SetException(new NotSupportedException("foo"));
+
+            // Assert
+            await action.Should().ThrowAsync<XunitException>().WithMessage(
+                "Expected a <System.InvalidOperationException> to be thrown within 100ms,"
+                + " but found <System.NotSupportedException>:*with message \"foo\"*");
+        }
+
+        [Fact]
+        public async Task When_task_throws_unexpected_exception_asynchronous_it_should_fail()
+        {
+            // Arrange
+            var timer = new FakeClock();
+            var taskFactory = new TaskCompletionSource<bool>();
+
+            // Act
+            Func<Task> action = () =>
+            {
+                return Awaiting(() => (Task)taskFactory.Task)
+                    .Should(timer).ThrowWithinAsync<InvalidOperationException>(1.Seconds());
+            };
+            _ = action.Invoke();
+            taskFactory.SetException(new NotSupportedException("foo"));
+
+            // Assert
+            await action.Should().ThrowAsync<XunitException>().WithMessage(
+                "Expected a <System.InvalidOperationException> to be thrown within 1s,"
+                + " but found <System.NotSupportedException>:*with message \"foo\"*");
         }
     }
 
