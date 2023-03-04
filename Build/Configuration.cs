abb8bbfd514962a18995ce3240f1ad9f03c6fcb3
Jonas Nyrup <jnyrup@gmail.com>
2023-03-04 10:46:54 +0100

    Target typed new expression

--- Build/Configuration.cs
+++ Build/Configuration.cs
@@ -4,9 +4,9 @@ using Nuke.Common.Tooling;
 [TypeConverter(typeof(TypeConverter<Configuration>))]
 public class Configuration : Enumeration
 {
-    public static Configuration Debug = new Configuration { Value = nameof(Debug) };
-    public static Configuration Release = new Configuration { Value = nameof(Release) };
-    public static Configuration CI = new Configuration { Value = nameof(CI) };
+    public static Configuration Debug = new() { Value = nameof(Debug) };
+    public static Configuration Release = new() { Value = nameof(Release) };
+    public static Configuration CI = new() { Value = nameof(CI) };
 
     public static implicit operator string(Configuration configuration)
     {
