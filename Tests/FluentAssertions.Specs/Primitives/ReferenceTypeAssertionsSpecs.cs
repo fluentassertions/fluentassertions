765f14a4a8211db68bccb9c9b3c92f09dd806fe8
Ben Arnold <ben@arnoldfamily.me.uk>
2023-03-12 20:17:42 +0000

    Do not add newlines to output for string values.

--- Tests/FluentAssertions.Specs/Primitives/ReferenceTypeAssertionsSpecs.cs
+++ Tests/FluentAssertions.Specs/Primitives/ReferenceTypeAssertionsSpecs.cs
@@ -42,8 +42,8 @@ public class ReferenceTypeAssertionsSpecs
         act
             .Should().Throw<XunitException>()
             .WithMessage(
-                "Expected subject to refer to*{*UserName =*JohnDoe*} because " +
-                "they are the same, but found*{*Name =*John Doe*}.");
+                @"Expected subject to refer to*{*UserName = ""JohnDoe""*} because " +
+                @"they are the same, but found*{*Name = ""John Doe""*}.");
     }
 
     [Fact]
