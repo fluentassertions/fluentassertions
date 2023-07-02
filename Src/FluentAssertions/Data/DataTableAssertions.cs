a832de3ed8987188d13cf4ae33bf4413e6137f9a
Jonas Nyrup <jnyrup@gmail.com>
2023-07-02 13:32:45 +0200

    Redundant using directive

--- Src/FluentAssertions/Data/DataTableAssertions.cs
+++ Src/FluentAssertions/Data/DataTableAssertions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
-using System.Xml.Schema;
 using FluentAssertions.Common;
 using FluentAssertions.Equivalency;
 using FluentAssertions.Execution;
