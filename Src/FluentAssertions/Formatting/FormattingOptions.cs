887cb62b110f067f4610e5a5503df5a6654f5390
Jonas Nyrup <jnyrup@gmail.com>
2023-07-22 10:26:27 +0200

    Update docs about thread safety

--- Src/FluentAssertions/Formatting/FormattingOptions.cs
+++ Src/FluentAssertions/Formatting/FormattingOptions.cs
@@ -5,11 +5,19 @@ public class FormattingOptions
     /// <summary>
     /// Indicates whether the formatter should use line breaks when the <see cref="IValueFormatter"/> supports it.
     /// </summary>
+    /// <remarks>
+    /// This value should not changed on <see cref="AssertionOptions.FormattingOptions"/> from within a unit test.
+    /// See the <see href="https://fluentassertions.com/extensibility/#thread-safety">docs</see> on how to safely use it.
+    /// </remarks>
     public bool UseLineBreaks { get; set; }
 
     /// <summary>
     /// Determines the depth until which the library should try to render an object graph.
     /// </summary>
+    /// <remarks>
+    /// This value should not changed on <see cref="AssertionOptions.FormattingOptions"/> from within a unit test.
+    /// See the <see href="https://fluentassertions.com/extensibility/#thread-safety">docs</see> on how to safely use it.
+    /// </remarks>
     /// <value>
     /// A depth of 1 will only the display the members of the root object.
     /// </value>
@@ -19,7 +27,13 @@ public class FormattingOptions
     /// Sets the maximum number of lines of the failure message.
     /// </summary>
     /// <remarks>
+    /// <para>
     /// Because of technical reasons, the actual output may be one or two lines longer.
+    /// </para>
+    /// <para>
+    /// This value should not changed on <see cref="AssertionOptions.FormattingOptions"/> from within a unit test.
+    /// See the <see href="https://fluentassertions.com/extensibility/#thread-safety">docs</see> on how to safely use it.
+    /// </para>
     /// </remarks>
     public int MaxLines { get; set; } = 100;
 
