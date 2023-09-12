e54ac716deae3357edc6cf68fd87996951ba1e5e
Valentin Breuß <v.breuss@tig.at>
2023-09-12 08:06:44 +0200

    Document the `IStringComparisonStrategy` interface

--- Src/FluentAssertions/Primitives/IStringComparisonStrategy.cs
+++ Src/FluentAssertions/Primitives/IStringComparisonStrategy.cs
@@ -7,7 +7,13 @@ namespace FluentAssertions.Primitives;
 /// </summary>
 internal interface IStringComparisonStrategy
 {
+    /// <summary>
+    /// The prefix for the message when the assertion fails.
+    /// </summary>
     string ExpectationDescription { get; }
 
+    /// <summary>
+    /// Asserts that the <paramref name="subject"/> matches the <paramref name="expected"/> value.
+    /// </summary>
     void ValidateAgainstMismatch(IAssertionScope assertion, string subject, string expected);
 }
