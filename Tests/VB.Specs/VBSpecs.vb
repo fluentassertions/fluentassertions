c4135cb6783bdec9eb780395f9111203516b50b1
Dennis Doomen <dennis.doomen@evision-software.com>
2023-09-20 08:41:07 +0200

    Fixed a namespace issue reported by Qodana

--- Tests/VB.Specs/VBSpecs.vb
+++ Tests/VB.Specs/VBSpecs.vb
@@ -2,31 +2,28 @@ Imports FluentAssertions
 Imports Xunit
 Imports Xunit.Sdk
 
-Namespace VB.Specs
-    Public Class VBSpecs
-        <Fact>
-        Public Sub Caller_identification_works_with_parentheses()
-            ' Arrange
-            Const subject = False
+Public Class VBSpecs
+    <Fact>
+    Public Sub Caller_identification_works_with_parentheses()
+        ' Arrange
+        Const subject = False
 
-            ' Act
-            Dim act As Action = Sub() subject.Should().BeTrue()
+        ' Act
+        Dim act As Action = Sub() subject.Should().BeTrue()
 
-            ' Assert
-            act.Should().Throw(Of XunitException).WithMessage("Expected subject to be true, but found false.")
-        End Sub
+        ' Assert
+        act.Should().Throw(Of XunitException).WithMessage("Expected subject to be true, but found false.")
+    End Sub
 
-        <Fact>
-        Public Sub Caller_identification_works_without_parentheses()
-            ' Arrange
-            Const subject = False
+    <Fact>
+    Public Sub Caller_identification_works_without_parentheses()
+        ' Arrange
+        Const subject = False
 
-            ' Act
-            Dim act As Action = Sub() subject.Should.BeTrue()
+        ' Act
+        Dim act As Action = Sub() subject.Should.BeTrue()
 
-            ' Assert
-            act.Should().Throw(Of XunitException).WithMessage("Expected subject to be true, but found false.")
-        End Sub
-    End Class
-End Namespace
-
+        ' Assert
+        act.Should().Throw(Of XunitException).WithMessage("Expected subject to be true, but found false.")
+    End Sub
+End Class
