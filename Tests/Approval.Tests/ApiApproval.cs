db26da6d3e32ad0dd5e72edb4bd785c431adea3a
Cédric Luthi <cedric.luthi@gmail.com>
2023-10-13 10:51:10 +0200

    Simplify the ApiApproval test (1/2)
    Simplify how paths relative to the solution directory are accessed.

--- Tests/Approval.Tests/ApiApproval.cs
+++ Tests/Approval.Tests/ApiApproval.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -23,19 +23,9 @@ public class ApiApproval
     [ClassData(typeof(TargetFrameworksTheoryData))]
     public Task ApproveApi(string frameworkVersion)
     {
-        string codeBase = Assembly.GetExecutingAssembly().Location;
-        var uri = new UriBuilder(new Uri(codeBase));
-        string assemblyPath = Uri.UnescapeDataString(uri.Path);
-        var containingDirectory = Path.GetDirectoryName(assemblyPath);
-        var configurationName = new DirectoryInfo(containingDirectory).Parent.Name;
-
-        var assemblyFile = Path.GetFullPath(
-            Path.Combine(
-                GetSourceDirectory(),
-                Path.Combine("..", "..", "Src", "FluentAssertions", "bin", configurationName, frameworkVersion,
-                    "FluentAssertions.dll")));
-
-        var assembly = Assembly.LoadFile(Path.GetFullPath(assemblyFile));
+        var configuration = typeof(ApiApproval).Assembly.GetCustomAttribute<AssemblyConfigurationAttribute>()!.Configuration;
+        var assemblyFile = GetPath("Src", "FluentAssertions", "bin", configuration, frameworkVersion, "FluentAssertions.dll");
+        var assembly = Assembly.LoadFile(assemblyFile);
         var publicApi = assembly.GeneratePublicApi(options: null);
 
         return Verifier
@@ -47,7 +37,9 @@ public class ApiApproval
             .DisableDiff();
     }
 
-    private static string GetSourceDirectory([CallerFilePath] string path = "") => Path.GetDirectoryName(path);
+    private static string GetSolutionDirectory([CallerFilePath] string path = "") => Path.Combine(Path.GetDirectoryName(path)!, "..", "..");
+
+    private static string GetPath(params string[] paths) => Path.GetFullPath(Path.Combine(paths.Prepend(GetSolutionDirectory()).ToArray()));
 
     // Copied from https://github.com/VerifyTests/Verify.DiffPlex/blob/master/src/Verify.DiffPlex/VerifyDiffPlex.cs
     public static Task<CompareResult> OnlyIncludeChanges(string received, string verified, IReadOnlyDictionary<string, object> _)
@@ -82,9 +74,7 @@ public class ApiApproval
     {
         public TargetFrameworksTheoryData()
         {
-            var csproj = Path.Combine(GetSourceDirectory(),
-                Path.Combine("..", "..", "Src", "FluentAssertions", "FluentAssertions.csproj"));
-
+            var csproj = GetPath("Src", "FluentAssertions", "FluentAssertions.csproj");
             var project = XDocument.Load(csproj);
             var targetFrameworks = project.XPathSelectElement("/Project/PropertyGroup/TargetFrameworks");
 
