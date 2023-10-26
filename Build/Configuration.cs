2059c96eeb6bcb78e1a63656eb58e170b8a3d424
Jonas Nyrup <jnyrup@gmail.com>
2023-10-26 19:36:28 +0200

    Fix MA0069 and SA1401

--- Build/Configuration.cs
+++ Build/Configuration.cs
@@ -4,9 +4,9 @@ using Nuke.Common.Tooling;
 [TypeConverter(typeof(TypeConverter<Configuration>))]
 public class Configuration : Enumeration
 {
-    public static Configuration Debug = new() { Value = nameof(Debug) };
+    public static readonly Configuration Debug = new() { Value = nameof(Debug) };
 
-    public static Configuration CI = new() { Value = nameof(CI) };
+    public static readonly Configuration CI = new() { Value = nameof(CI) };
 
     public static implicit operator string(Configuration configuration)
     {
