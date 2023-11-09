e1ce227f619f432eae696255d08d005613c200a2
Valentin Breuß <vbreuss@gmail.com>
2023-11-09 13:43:00 +0100

    Add tests to avoid Qodana issue "Method 'xxx' return value is never used"

---
+++ Tests/FluentAssertions.Specs/Specialized/ActionAssertionSpecs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace FluentAssertions.Specs.Specialized;
+
+public class ActionAssertionSpecs
+{
+    public class NotThrow
+    {
+        [Fact]
+        public void Allow_additional_assertions_on_return_value()
+        {
+            // Arrange
+            Action subject = () => { };
+
+            // Act / Assert
+            subject.Should().NotThrow()
+                .And.NotBeNull();
+        }
+    }
+}
--- Tests/FluentAssertions.Specs/Specialized/DelegateAssertionSpecs.cs
+++ Tests/FluentAssertions.Specs/Specialized/DelegateAssertionSpecs.cs
@@ -7,37 +7,52 @@ namespace FluentAssertions.Specs.Specialized;
 
 public class DelegateAssertionSpecs
 {
-    [Fact]
-    public void When_injecting_a_null_extractor_it_should_throw()
+    public class Throw
     {
-        // Arrange
-        Action subject = () => { };
+        [Fact]
+        public void Allow_additional_assertions_on_return_value()
+        {
+            // Arrange
+            var exception = new Exception("foo");
+            Action subject = () => throw exception;
 
-        // Act
-        Func<ActionAssertions> act = () => new ActionAssertions(subject, extractor: null);
-
-        // Act
-        act.Should().ThrowExactly<ArgumentNullException>()
-            .WithParameterName("extractor");
-    }
-
-    [Fact]
-    public void When_injecting_a_null_clock_it_should_throw()
-    {
-        // Arrange
-        Action subject = () => { };
-        IExtractExceptions extractor = new AggregateExceptionExtractor();
-
-        // Act
-        Func<ActionAssertions> act = () => new ActionAssertions(subject, extractor, clock: null);
-
-        // Act
-        act.Should().ThrowExactly<ArgumentNullException>()
-            .WithParameterName("clock");
+            // Act / Assert
+            subject.Should().Throw<Exception>()
+                .Which.Message.Should().Be("foo");
+        }
     }
 
     public class ThrowExactly
     {
+        [Fact]
+        public void When_injecting_a_null_extractor_it_should_throw()
+        {
+            // Arrange
+            Action subject = () => { };
+
+            // Act
+            Func<ActionAssertions> act = () => new ActionAssertions(subject, extractor: null);
+
+            // Act
+            act.Should().ThrowExactly<ArgumentNullException>()
+                .WithParameterName("extractor");
+        }
+
+        [Fact]
+        public void When_injecting_a_null_clock_it_should_throw()
+        {
+            // Arrange
+            Action subject = () => { };
+            IExtractExceptions extractor = new AggregateExceptionExtractor();
+
+            // Act
+            Func<ActionAssertions> act = () => new ActionAssertions(subject, extractor, clock: null);
+
+            // Act
+            act.Should().ThrowExactly<ArgumentNullException>()
+                .WithParameterName("clock");
+        }
+
         [Fact]
         public void Does_not_continue_assertion_on_exact_exception_type()
         {
