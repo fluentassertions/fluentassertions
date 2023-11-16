6500e466c93726fe062eb9f630e0a3043c3a1c14
Lukas Gasselsberger | alu-one <l.gasselsberger@alu-one.at>
2023-11-16 12:45:26 +0100

    Disable all Qodana rules for polyfills

--- Src/FluentAssertions/Polyfill/StringBuilderExtensions.cs
+++ Src/FluentAssertions/Polyfill/StringBuilderExtensions.cs
@@ -9,7 +9,7 @@ namespace System.Text;
 /// Since net6.0 StringBuilder has additional overloads taking an AppendInterpolatedStringHandler
 /// and optionally an IFormatProvider.
 /// The overload here is polyfill for older target frameworks to avoid littering the code base with #ifs
-/// in order to silence analyzers about dependending on the current culture instead of an invariant culture.
+/// in order to silence analyzers about depending on the current culture instead of an invariant culture.
 /// </summary>
 internal static class StringBuilderExtensions
 {
