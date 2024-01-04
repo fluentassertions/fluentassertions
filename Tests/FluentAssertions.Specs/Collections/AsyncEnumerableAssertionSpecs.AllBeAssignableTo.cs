f80486f17d0b3096d8e4d18e6a3946c65888ec12
Zach Saw <zach.saw@loanmarketgroup.com>
2024-01-04 14:29:03 +1100

    Issue-1213: Add unit tests

---
+++ Tests/FluentAssertions.Specs/Collections/AsyncEnumerableAssertionSpecs.AllBeAssignableTo.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions.Execution;
+using Xunit;
+using Xunit.Sdk;
+
+namespace FluentAssertions.Specs.Collections;
+
+/// <content>
+/// The AllBeAssignableTo specs.
+/// </content>
+public partial class AsyncEnumerableAssertionSpecs
+{
+    public class AllBeAssignableTo
+    {
+        [Fact]
+        public void When_the_types_in_a_collection_is_matched_against_a_null_type_it_should_throw()
+        {
+            // Arrange
+            int[] collection = [];
+
+            // Act
+            Action act = () => collection.ToAsyncEnumerable().Should().AllBeAssignableTo(null);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>()
+                .WithParameterName("expectedType");
+        }
+
+        [Fact]
+        public void All_items_in_an_empty_collection_are_assignable_to_a_generic_type()
+        {
+            // Arrange
+            int[] collection = [];
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo<int>();
+        }
+
+        [Fact]
+        public void When_collection_is_null_then_all_be_assignable_to_should_fail()
+        {
+            // Arrange
+            IAsyncEnumerable<object> collection = null;
+
+            // Act
+            Action act = () =>
+            {
+                using var _ = new AssertionScope();
+                collection.Should().AllBeAssignableTo(typeof(object), "we want to test the failure {0}", "message");
+            };
+
+            // Assert
+            act.Should().Throw<XunitException>()
+                .WithMessage("Expected type to be \"*.Object\" *failure message*, but found collection is <null>.");
+        }
+
+        [Fact]
+        public void All_items_in_an_empty_collection_are_assignable_to_a_type()
+        {
+            // Arrange
+            int[] collection = [];
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo(typeof(int));
+        }
+
+        [Fact]
+        public void When_all_of_the_types_in_a_collection_match_expected_type_it_should_succeed()
+        {
+            // Arrange
+            int[] collection = [1, 2, 3];
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo(typeof(int));
+        }
+
+        [Fact]
+        public void When_all_of_the_types_in_a_collection_match_expected_generic_type_it_should_succeed()
+        {
+            // Arrange
+            int[] collection = [1, 2, 3];
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo<int>();
+        }
+
+        [Fact]
+        public void When_matching_a_collection_against_a_type_it_should_return_the_casted_items()
+        {
+            // Arrange
+            int[] collection = [1, 2, 3];
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo<int>()
+                .Which.Should().Equal(1, 2, 3);
+        }
+
+        [Fact]
+        public void When_all_of_the_types_in_a_collection_match_the_type_or_subtype_it_should_succeed()
+        {
+            // Arrange
+            var collection = new object[] { new Exception(), new ArgumentException("foo") };
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo<Exception>();
+        }
+
+        [Fact]
+        public void When_one_of_the_types_does_not_match_it_should_throw_with_a_clear_explanation()
+        {
+            // Arrange
+            var collection = new object[] { 1, "2", 3 };
+
+            // Act
+            Action act = () => collection.ToAsyncEnumerable().Should().AllBeAssignableTo(typeof(int), "because they are of different type");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Expected type to be \"System.Int32\" because they are of different type, but found \"[System.Int32, System.String, System.Int32]\".");
+        }
+
+        [Fact]
+        public void When_one_of_the_types_does_not_match_the_generic_type_it_should_throw_with_a_clear_explanation()
+        {
+            // Arrange
+            var collection = new object[] { 1, "2", 3 };
+
+            // Act
+            Action act = () => collection.ToAsyncEnumerable().Should().AllBeAssignableTo<int>("because they are of different type");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Expected type to be \"System.Int32\" because they are of different type, but found \"[System.Int32, System.String, System.Int32]\".");
+        }
+
+        [Fact]
+        public void When_one_of_the_elements_is_null_it_should_throw_with_a_clear_explanation()
+        {
+            // Arrange
+            var collection = new object[] { 1, null, 3 };
+
+            // Act
+            Action act = () => collection.ToAsyncEnumerable().Should().AllBeAssignableTo<int>("because they are of different type");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Expected type to be \"System.Int32\" because they are of different type, but found a null element.");
+        }
+
+        [Fact]
+        public void When_collection_is_of_matching_types_it_should_succeed()
+        {
+            // Arrange
+            Type[] collection = [typeof(Exception), typeof(ArgumentException)];
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo<Exception>();
+        }
+
+        [Fact]
+        public void When_collection_of_types_contains_one_type_that_does_not_match_it_should_throw_with_a_clear_explanation()
+        {
+            // Arrange
+            Type[] collection = [typeof(int), typeof(string), typeof(int)];
+
+            // Act
+            Action act = () => collection.ToAsyncEnumerable().Should().AllBeAssignableTo<int>("because they are of different type");
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "Expected type to be \"System.Int32\" because they are of different type, but found \"[System.Int32, System.String, System.Int32]\".");
+        }
+
+        [Fact]
+        public void When_collection_of_types_and_objects_are_all_of_matching_types_it_should_succeed()
+        {
+            // Arrange
+            var collection = new object[] { typeof(int), 2, typeof(int) };
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo<int>();
+        }
+
+        [Fact]
+        public void When_collection_of_different_types_and_objects_are_all_assignable_to_type_it_should_succeed()
+        {
+            // Arrange
+            var collection = new object[] { typeof(Exception), new ArgumentException("foo") };
+
+            // Act / Assert
+            collection.ToAsyncEnumerable().Should().AllBeAssignableTo<Exception>();
+        }
+
+        [Fact]
+        public void When_collection_is_null_then_all_be_assignable_toOfT_should_fail()
+        {
+            // Arrange
+            IAsyncEnumerable<object> collection = null;
+
+            // Act
+            Action act = () =>
+            {
+                using var _ = new AssertionScope();
+                collection.Should().AllBeAssignableTo<object>("we want to test the failure {0}", "message");
+            };
+
+            // Assert
+            act.Should().Throw<XunitException>()
+                .WithMessage("Expected type to be \"*.Object\" *failure message*, but found collection is <null>.");
+        }
+    }
+}
---
+++ Tests/FluentAssertions.Specs/Collections/AsyncEnumerableAssertionSpecs.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace FluentAssertions.Specs.Collections;
+
+/// <summary>
+/// Collection assertion specs.
+/// </summary>
+public partial class AsyncEnumerableAssertionSpecs
+{
+    public class Chainings
+    {
+        [Fact]
+        public void Should_support_chaining_constraints_with_and()
+        {
+            // Arrange
+            int[] collection = [1, 2, 3];
+
+            // Act / Assert
+            collection.ToAsyncEnumerable()
+                .Should()
+                .HaveCount(3)
+                .And
+                .HaveElementAt(1, 2)
+                .And
+                .NotContain(4);
+        }
+
+        [Fact]
+        public void When_the_collection_is_ordered_according_to_the_subsequent_ascending_assertion_it_should_succeed()
+        {
+            // Arrange
+            (int, string)[] collection =
+            [
+                (1, "a"),
+                (2, "b"),
+                (2, "c"),
+                (3, "a")
+            ];
+
+            // Act
+            Action action = () => collection.ToAsyncEnumerable()
+                .Should()
+                .BeInAscendingOrder(x => x.Item1)
+                .And
+                .ThenBeInAscendingOrder(x => x.Item2);
+
+            // Assert
+            action.Should().NotThrow();
+        }
+
+        [Fact]
+        public void When_the_collection_is_not_ordered_according_to_the_subsequent_ascending_assertion_it_should_fail()
+        {
+            // Arrange
+            (int, string)[] tuples =
+            [
+                (1, "a"),
+                (2, "b"),
+                (2, "c"),
+                (3, "a")
+            ];
+
+            var collection = tuples.ToAsyncEnumerable();
+
+            // Act
+            Action action = () => collection.Should()
+                .BeInAscendingOrder(x => x.Item1)
+                .And
+                .BeInAscendingOrder(x => x.Item2);
+
+            // Assert
+            action.Should().Throw<XunitException>()
+                .WithMessage("Expected collection * to be ordered \"by Item2\"*");
+        }
+
+        [Fact]
+        public void
+            When_the_collection_is_ordered_according_to_the_subsequent_ascending_assertion_with_comparer_it_should_succeed()
+        {
+            // Arrange
+            (int, string)[] collection =
+            [
+                (1, "a"),
+                (2, "B"),
+                (2, "b"),
+                (3, "a")
+            ];
+
+            // Act
+            Action action = () => collection.ToAsyncEnumerable()
+                .Should()
+                .BeInAscendingOrder(x => x.Item1)
+                .And
+                .ThenBeInAscendingOrder(x => x.Item2, StringComparer.InvariantCultureIgnoreCase);
+
+            // Assert
+            action.Should().NotThrow();
+        }
+
+        [Fact]
+        public void When_the_collection_is_ordered_according_to_the_multiple_subsequent_ascending_assertions_it_should_succeed()
+        {
+            // Arrange
+            (int, string, double)[] collection =
+            [
+                (1, "a", 1.1),
+                (2, "b", 1.2),
+                (2, "c", 1.3),
+                (3, "a", 1.1)
+            ];
+
+            // Act
+            Action action = () => collection.ToAsyncEnumerable()
+                .Should()
+                .BeInAscendingOrder(x => x.Item1)
+                .And
+                .ThenBeInAscendingOrder(x => x.Item2)
+                .And
+                .ThenBeInAscendingOrder(x => x.Item3);
+
+            // Assert
+            action.Should().NotThrow();
+        }
+
+        [Fact]
+        public void When_the_collection_is_ordered_according_to_the_subsequent_descending_assertion_it_should_succeed()
+        {
+            // Arrange
+            (int, string)[] collection =
+            [
+                (3, "a"),
+                (2, "c"),
+                (2, "b"),
+                (1, "a")
+            ];
+
+            // Act
+            Action action = () => collection.ToAsyncEnumerable()
+                .Should()
+                .BeInDescendingOrder(x => x.Item1)
+                .And
+                .ThenBeInDescendingOrder(x => x.Item2);
+
+            // Assert
+            action.Should().NotThrow();
+        }
+
+        [Fact]
+        public void When_the_collection_is_not_ordered_according_to_the_subsequent_descending_assertion_it_should_fail()
+        {
+            // Arrange
+            (int, string)[] tuples =
+            [
+                (3, "a"),
+                (2, "c"),
+                (2, "b"),
+                (1, "a")
+            ];
+
+            var collection = tuples.ToAsyncEnumerable();
+
+            // Act
+            Action action = () => collection.Should()
+                .BeInDescendingOrder(x => x.Item1)
+                .And
+                .BeInDescendingOrder(x => x.Item2);
+
+            // Assert
+            action.Should().Throw<XunitException>()
+                .WithMessage("Expected collection * to be ordered \"by Item2\"*");
+        }
+
+        [Fact]
+        public void
+            When_the_collection_is_ordered_according_to_the_subsequent_descending_assertion_with_comparer_it_should_succeed()
+        {
+            // Arrange
+            (int, string)[] collection =
+            [
+                (3, "a"),
+                (2, "b"),
+                (2, "B"),
+                (1, "a")
+            ];
+
+            // Act
+            Action action = () => collection.ToAsyncEnumerable()
+                .Should()
+                .BeInDescendingOrder(x => x.Item1)
+                .And
+                .ThenBeInDescendingOrder(x => x.Item2, StringComparer.InvariantCultureIgnoreCase);
+
+            // Assert
+            action.Should().NotThrow();
+        }
+
+        [Fact]
+        public void When_the_collection_is_ordered_according_to_the_multiple_subsequent_descending_assertions_it_should_succeed()
+        {
+            // Arrange
+            (int, string, double)[] collection =
+            [
+                (3, "a", 1.1),
+                (2, "c", 1.3),
+                (2, "b", 1.2),
+                (1, "a", 1.1)
+            ];
+
+            // Act
+            Action action = () => collection.ToAsyncEnumerable()
+                .Should()
+                .BeInDescendingOrder(x => x.Item1)
+                .And
+                .ThenBeInDescendingOrder(x => x.Item2)
+                .And
+                .ThenBeInDescendingOrder(x => x.Item3);
+
+            // Assert
+            action.Should().NotThrow();
+        }
+    }
+
+    private class ByLastCharacterComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return x.Last().CompareTo(y.Last());
+        }
+    }
+}
+
+internal class CountingAsyncEnumerable<TElement> : IEnumerable<TElement>
+{
+    private readonly IEnumerable<TElement> backingSet;
+
+    public CountingAsyncEnumerable(IEnumerable<TElement> backingSet)
+    {
+        this.backingSet = backingSet;
+        GetEnumeratorCallCount = 0;
+    }
+
+    public int GetEnumeratorCallCount { get; private set; }
+
+    public IEnumerator<TElement> GetEnumerator()
+    {
+        GetEnumeratorCallCount++;
+        return backingSet.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
