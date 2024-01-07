8a4b4ff87b7a9facf07e55c2137b9be79ad683b0
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2024-01-07 09:19:37 +0100

    Added an extra assertion engine initializer that throws for extra coverage

--- Tests/FluentAssertions.Extensibility.Specs/AssertionEngineInitializer.cs
+++ Tests/FluentAssertions.Extensibility.Specs/AssertionEngineInitializer.cs
@@ -1,9 +1,14 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 // With specific initialization code to invoke before the first assertion happens
 [assembly: FluentAssertions.Extensibility.AssertionEngineInitializer(
     typeof(FluentAssertions.Extensibility.Specs.AssertionEngineInitializer),
     nameof(FluentAssertions.Extensibility.Specs.AssertionEngineInitializer.InitializeBeforeFirstAssertion))]
+
+[assembly: FluentAssertions.Extensibility.AssertionEngineInitializer(
+    typeof(FluentAssertions.Extensibility.Specs.AssertionEngineInitializer),
+    nameof(FluentAssertions.Extensibility.Specs.AssertionEngineInitializer.InitializeBeforeFirstAssertionButThrow))]
 
 namespace FluentAssertions.Extensibility.Specs;
 
@@ -16,5 +21,10 @@ public static class AssertionEngineInitializer
     public static void InitializeBeforeFirstAssertion()
     {
         Interlocked.Increment(ref shouldBeCalledOnlyOnce);
+    }
+
+    public static void InitializeBeforeFirstAssertionButThrow()
+    {
+        throw new InvalidOperationException("Bogus exception to make sure the engine ignores them");
     }
 }
