f629fc558bc3578f745d6ffb2f936054a47641a1
Jonas Nyrup <jnyrup@gmail.com>
2024-03-24 11:27:49 +0100

    Only allocate Lazy<> and lambda when necessary
    The local function avoid allocating a lambda when not used.
    For more details, see: https://github.com/dotnet/roslyn/issues/20777

--- Tests/FluentAssertions.Specs/Execution/AssertionScope.MessageFormatingSpecs.cs
+++ Tests/FluentAssertions.Specs/Execution/AssertionScope.MessageFormatingSpecs.cs
@@ -83,6 +83,22 @@ public partial class AssertionScopeSpecs
     }
 
     [Fact]
+    public void The_inner_scope_is_used_when_the_outer_scope_does_not_have_a_context()
+    {
+        // Act
+        Action act = () =>
+        {
+            using var outerScope = new AssertionScope();
+            using var innerScope = new AssertionScope("inner");
+            new[] { 1, 2, 3 }.Should().Equal(3, 2, 1);
+        };
+
+        // Assert
+        act.Should().Throw<XunitException>()
+            .WithMessage("Expected inner to be equal to*");
+    }
+
+    [Fact]
     public void Message_should_contain_each_unique_failed_assertion_seperately()
     {
         // Act
