2ae2720f83106f2d110e131a8bb915fbadf31678
Lukas Gasselsberger | alu-one <l.gasselsberger@alu-one.at>
2024-06-20 13:29:42 +0200

    Improve code coverage in `TaskFormatter`

--- Src/FluentAssertions/Formatting/TaskFormatter.cs
+++ Src/FluentAssertions/Formatting/TaskFormatter.cs
@@ -3,7 +3,7 @@
 namespace FluentAssertions.Formatting;
 
 /// <summary>
-/// Provides a human readable version of a generic or non-generic <see cref="Task"/>
+/// Provides a human-readable version of a generic or non-generic <see cref="Task"/>
 /// including its state.
 /// </summary>
 public class TaskFormatter : IValueFormatter
@@ -15,14 +15,8 @@ public class TaskFormatter : IValueFormatter
 
     public void Format(object value, FormattedObjectGraph formattedGraph, FormattingContext context, FormatChild formatChild)
     {
-        if (value is Task task)
-        {
-            formatChild("type", task.GetType(), formattedGraph);
-            formattedGraph.AddFragment($" {{Status={task.Status}}}");
-        }
-        else
-        {
-            formattedGraph.AddFragment("<null>");
-        }
+        var task = (Task)value;
+        formatChild("type", task.GetType(), formattedGraph);
+        formattedGraph.AddFragment($" {{Status={task.Status}}}");
     }
 }
