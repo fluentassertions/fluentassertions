c6b825bdb7e9bbf7b52102bb0c4c1c02b654c1e6
Cédric Luthi <cedric.luthi@gmail.com>
2024-08-05 10:20:11 +0200

    Add support for xUnit.net v3
    Fixes #2709

--- Src/FluentAssertions/Execution/TestFrameworkProvider.cs
+++ Src/FluentAssertions/Execution/TestFrameworkProvider.cs
@@ -18,7 +18,8 @@ internal class TestFrameworkProvider
         ["mspec"] = new MSpecFramework(),
         ["nunit"] = new NUnitTestFramework(),
         ["mstestv2"] = new MSTestFrameworkV2(),
-        ["xunit2"] = new XUnit2TestFramework() // Keep this the last one as it uses a try/catch approach
+        ["xunit2"] = new XUnitTestFramework("xunit.assert"),
+        ["xunit3"] = new XUnitTestFramework("xunit.v3.assert"), // Keep XUnitTestFramework last as it uses a try/catch approach
     };
 
     private readonly Configuration configuration;
---
+++ Tests/TestFrameworks/XUnit3.Specs/FrameworkSpecs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using Xunit.Sdk;
+
+namespace XUnit3.Specs;
+
+public class FrameworkSpecs
+{
+    [Fact]
+    public void When_xunit3_is_used_it_should_throw_xunit_exceptions_for_assertion_failures()
+    {
+        // Act
+        Action act = () => 0.Should().Be(1);
+
+        // Assert
+        act.Should().Throw<XunitException>();
+    }
+}
