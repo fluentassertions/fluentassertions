9d990f266712ab1576f4ff8fc58b1722289f5a0d
Cédric Luthi <cedric.luthi@gmail.com>
2024-08-21 21:35:46 +0200

    Improve implementation of LateBoundTestFramework
    Also simplify the implementation of XUnitTestFramework by inheriting LateBoundTestFramework.

--- Src/FluentAssertions/Execution/XUnitTestFramework.cs
+++ Src/FluentAssertions/Execution/XUnitTestFramework.cs
@@ -1,37 +1,11 @@
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-
-namespace FluentAssertions.Execution;
+﻿namespace FluentAssertions.Execution;
 
 /// <summary>
 /// Implements the xUnit (version 2 and 3) test framework adapter.
 /// </summary>
-internal class XUnitTestFramework(string assemblyName) : ITestFramework
+internal class XUnitTestFramework(string assemblyName) : LateBoundTestFramework(loadAssembly: true)
 {
-    private Type exceptionType;
+    protected internal override string AssemblyName => assemblyName;
 
-    public bool IsAvailable
-    {
-        get
-        {
-            try
-            {
-                // For netfx the assembly is not in AppDomain by default, so we can't just scan AppDomain.CurrentDomain
-                exceptionType = Assembly.Load(new AssemblyName(assemblyName)).GetType("Xunit.Sdk.XunitException");
-
-                return exceptionType is not null;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-    }
-
-    [DoesNotReturn]
-    public void Throw(string message)
-    {
-        throw (Exception)Activator.CreateInstance(exceptionType, message);
-    }
+    protected override string ExceptionFullName => "Xunit.Sdk.XunitException";
 }
