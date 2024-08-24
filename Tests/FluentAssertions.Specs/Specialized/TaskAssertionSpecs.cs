2181217c15f8be0d01b280ff2d69446e7dcc3c4d
Jonas Nyrup <jnyrup@gmail.com>
2024-08-24 18:49:24 +0200

    Remove unused usings

--- Tests/FluentAssertions.Specs/Specialized/TaskAssertionSpecs.cs
+++ Tests/FluentAssertions.Specs/Specialized/TaskAssertionSpecs.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions.Execution;
 using FluentAssertions.Extensions;
-using FluentAssertions.Specialized;
 using Xunit;
 using Xunit.Sdk;
 
