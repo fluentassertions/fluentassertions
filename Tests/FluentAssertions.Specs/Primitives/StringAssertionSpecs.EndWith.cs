93fe387f586cd6ebd612681af0efa8d3f875392d
Lukas Gasselsberger | alu-one <l.gasselsberger@alu-one.at>
2024-10-26 11:59:39 +0200

    Add missing test when chaining two assertions for `StringStartStrategy` and `StringEndStrategy`

--- Tests/FluentAssertions.Specs/Primitives/StringAssertionSpecs.EndWith.cs
+++ Tests/FluentAssertions.Specs/Primitives/StringAssertionSpecs.EndWith.cs
@@ -84,6 +84,21 @@ public partial class StringAssertionSpecs
         }
 
         [Fact]
+        public void Correctly_stop_further_execution_when_inside_assertion_scope()
+        {
+            // Act
+            Action act = () =>
+            {
+                using var _ = new AssertionScope();
+                "ABC".Should().EndWith("00ABC").And.EndWith("CBA00");
+            };
+
+            // Assert
+            act.Should().Throw<XunitException>().WithMessage(
+                "*\"00ABC\"*");
+        }
+
+        [Fact]
         public void When_string_ending_is_compared_and_actual_value_is_null_then_it_should_throw()
         {
             // Arrange
