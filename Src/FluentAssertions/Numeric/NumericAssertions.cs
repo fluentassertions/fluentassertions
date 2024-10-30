bcf067439a9623cac2e82884402bcc4f88d5af2c
Valentin <valentin.breuss@baur.eu>
2024-10-30 09:34:47 +0100

    Fix Qodana issues about using directive not required by the code

--- Src/FluentAssertions/Numeric/NumericAssertions.cs
+++ Src/FluentAssertions/Numeric/NumericAssertions.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Linq.Expressions;
-using FluentAssertions.Common;
 using FluentAssertions.Execution;
 
 namespace FluentAssertions.Numeric;
