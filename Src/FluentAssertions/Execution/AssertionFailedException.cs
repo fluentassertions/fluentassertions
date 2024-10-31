97ce461272b383cb2c25f619061592f53f8da6f2
Lukas Gasselsberger | alu-one <l.gasselsberger@alu-one.at>
2024-10-31 08:59:12 +0100

    Fix XML comment in `AssertionFailedException`

--- Src/FluentAssertions/Execution/AssertionFailedException.cs
+++ Src/FluentAssertions/Execution/AssertionFailedException.cs
@@ -5,6 +5,7 @@ namespace FluentAssertions.Execution;
 /// <summary>
 /// Represents the default exception in case no test framework is configured.
 /// </summary>
+/// <param name="message">The <em>mandatory</em> exception message</param>
 #pragma warning disable CA1032, RCS1194 // AssertionFailedException should never be constructed with an empty message
 public class AssertionFailedException(string message) : Exception(message)
 #pragma warning restore CA1032, RCS1194
