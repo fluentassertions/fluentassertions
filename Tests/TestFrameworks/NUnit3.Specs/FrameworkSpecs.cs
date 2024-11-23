0273ed70262fe8e3b24a5a4c636021e77601938e
Cédric Luthi <cedric.luthi@gmail.com>
2024-11-23 14:50:47 +0100

    Add support for xUnit.net v3 (#2718)
    (cherry picked from commit 3e00296d7e62bd81418b2954a128a101f7da1018)

--- Tests/TestFrameworks/NUnit3.Specs/FrameworkSpecs.cs
+++ Tests/TestFrameworks/NUnit3.Specs/FrameworkSpecs.cs
@@ -15,8 +15,9 @@ public class FrameworkSpecs
 
         // Assert
         Exception exception = act.Should().Throw<Exception>().Which;
-        exception.GetType()
-            .FullName.Should()
-            .ContainEquivalentOf("NUnit.Framework.AssertionException");
+
+        // Don't reference the exception type explicitly like this: act.Should().Throw<AssertionException>()
+        // It could cause this specs project to load the assembly containing the exception (this actually happens for xUnit)
+        exception.GetType().FullName.Should().Be("NUnit.Framework.AssertionException");
     }
 }
--- Tests/TestFrameworks/XUnit3.Specs/FrameworkSpecs.cs
+++ Tests/TestFrameworks/XUnit3.Specs/FrameworkSpecs.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
-namespace XUnit2.Specs;
+namespace XUnit3.Specs;
 
 public class FrameworkSpecs
 {
     [Fact]
-    public void When_xunit2_is_used_it_should_throw_xunit_exceptions_for_assertion_failures()
+    public void When_xunit3_is_used_it_should_throw_xunit_exceptions_for_assertion_failures()
     {
         // Act
         Action act = () => 0.Should().Be(1);
@@ -17,6 +18,7 @@ public class FrameworkSpecs
 
         // Don't reference the exception type explicitly like this: act.Should().Throw<XunitException>()
         // It could cause this specs project to load the assembly containing the exception (this actually happens for xUnit)
+        exception.GetType().GetInterfaces().Select(e => e.Name).Should().Contain("IAssertionException");
         exception.GetType().FullName.Should().Be("Xunit.Sdk.XunitException");
     }
 }
