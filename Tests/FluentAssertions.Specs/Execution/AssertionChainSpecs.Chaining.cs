fluentassertions/fluentassertions

commit 7b53b8a82c36a48a7ae4bb14cd81c4f338187e41

Improves the subject identification for chained assertions and those that use Which

Src/FluentAssertions/Execution/SubjectIdentificationBuilder.cs
@@ -1,0 +1,102 @@
+using System;
+using FluentAssertions.Common;
+
+namespace FluentAssertions.Execution;
+
+/// <summary>
+/// Responsible for constructing the code phrase that identifies the subject-under-test and which is used
+/// in assertion failures managed by a single <see cref="AssertionChain"/>.
+/// </summary>
+/// <remarks>
+/// Takes into account the caller identifiers extracted from the invoking code, any scopes created through
+/// (nested) instances of <see cref="AssertionScope"/> and modifications made by the
+/// <see cref="AndWhichConstraint{TParent,TSubject}"/>.
+/// </remarks>
+internal class SubjectIdentificationBuilder
+{
+    private readonly Func<string> getScopeName;
+    private readonly Lazy<string[]> identifiersExtractedFromTheCode;
+    private int identifierIndex;
+    private Func<string> getSubject;
+
+    public SubjectIdentificationBuilder(Func<string[]> getCallerIdentifiers, Func<string> getScopeName)
+    {
+        this.getScopeName = getScopeName;
+
+        identifiersExtractedFromTheCode = new(() => getCallerIdentifiers());
+
+        getSubject = () => GetIdentifier(0);
+    }
+
+    /// <summary>
+    /// Tells the builder to assume the assertion has moved to the next identifier in a chained assertion.
+    /// </summary>
+    public void AdvanceToNextSubject()
+    {
+        identifierIndex++;
+        var localIndex = identifierIndex;
+
+        getSubject = () => GetIdentifier(localIndex);
+    }
+
+    /// <summary>
+    /// Indicates whether the subject identifier has been overridden with a custom value during the assertion process.
+    /// </summary>
+    /// <remarks>
+    /// When <see cref="OverrideSubjectIdentifier"/> or <see cref="UsePostfix(string)"/> is called,
+    /// this property is set to true, signaling that a manually defined identifier is now used instead of
+    /// automatically constructed identifiers.
+    /// </remarks>
+    public bool HasOverriddenIdentifier { get; private set; }
+
+    /// <summary>
+    /// Completely overrides the construction process with the specified lazy-evaluated identifier.
+    /// </summary>
+    public void OverrideSubjectIdentifier(Func<string> getSubject)
+    {
+        HasOverriddenIdentifier = true;
+        this.getSubject = getSubject;
+    }
+
+    /// <summary>
+    /// Appends the given postfix to the current subject and combines that with next subject.
+    /// </summary>
+    /// <remarks>
+    /// If the postfix is <c>[0]</c>, the current identifier is <c>collection</c>, and the next one
+    /// is <c>Parameters</c>, this method will assume the subject is <c>collection[0].Parameters</c>.
+    /// </remarks>
+    public void UsePostfix(string postfix)
+    {
+        var localIndex = identifierIndex;
+        getSubject = () => (GetIdentifier(localIndex) + postfix).Combine(GetIdentifier(localIndex + 1));
+
+        HasOverriddenIdentifier = true;
+    }
+
+    /// <summary>
+    /// Returns the constructed subject identifier.
+    /// </summary>
+    public string Build()
+    {
+        var scopeName = getScopeName();
+        var callerIdentifier = getSubject();
+
+        if (scopeName is null)
+        {
+            return callerIdentifier;
+        }
+        else if (callerIdentifier is null)
+        {
+            return scopeName;
+        }
+        else
+        {
+            return $"{scopeName}/{callerIdentifier}";
+        }
+    }
+
+    private string GetIdentifier(int index)
+    {
+        return identifiersExtractedFromTheCode.Value.Length > index ? identifiersExtractedFromTheCode.Value[index] : null;
+    }
+}

Src/FluentAssertions/AndWhichConstraint.cs
@@ -47,10 +47,27 @@ public class AndWhichConstraint<TParent, TSubject> : AndConstraint<TParent>
     /// <remarks>
     /// If <paramref name="subjects"/> contains more than one object, a clear exception is thrown.
     /// </remarks>
+    // REFACTOR: In a next major version, we need to remove this overload and make the AssertionChain required
     public AndWhichConstraint(TParent parent, IEnumerable<TSubject> subjects)
         : base(parent)
     {
         getSubject = new Lazy<TSubject>(() => Single(subjects));
+    }
+
+    /// <summary>
+    /// Creates an object that allows continuing an assertion executed through <paramref name="parent"/> and
+    /// which resulted in a potential collection of objects through <paramref name="subjects"/> on an
+    /// existing <paramref name="assertionChain"/>.
+    /// </summary>
+    /// <remarks>
+    /// If <paramref name="subjects"/> contains more than one object, a clear exception is thrown.
+    /// </remarks>
+    public AndWhichConstraint(TParent parent, IEnumerable<TSubject> subjects, AssertionChain assertionChain)
+        : base(parent)
+    {
+        getSubject = new Lazy<TSubject>(() => Single(subjects));
+
+        this.assertionChain = assertionChain;
     }
 
     /// <summary>
@@ -107,6 +124,12 @@ public class AndWhichConstraint<TParent, TSubject> : AndConstraint<TParent>
             if (pathPostfix is not null and not "")
             {
                 assertionChain.WithCallerPostfix(pathPostfix).ReuseOnce();
+            }
+            else
+            {
+                // Make sure the caller identification restarts with the code following the Which property.
+                assertionChain?.AdvanceToNextIdentifier();
+                assertionChain?.ReuseOnce();
             }
 
             return getSubject.Value;

Src/FluentAssertions/CallerIdentification/CallerStatementBuilder.cs
@@ -4,48 +4,62 @@ using System.Text;
 
 namespace FluentAssertions.CallerIdentification;
 
+// REFACTOR: This is not a builder, but a parser, so rename it accordingly.
 internal class CallerStatementBuilder
 {
     private readonly StringBuilder statement;
-    private readonly List<IParsingStrategy> priorityOrderedParsingStrategies;
-    private ParsingState parsingState = ParsingState.InProgress;
+    private readonly List<IParsingStrategy> parsingStrategies;
+    private readonly List<string> candidates = new();
+    private ParsingState state = ParsingState.InProgress;
 
     internal CallerStatementBuilder()
     {
         statement = new StringBuilder();
 
-        priorityOrderedParsingStrategies =
+        parsingStrategies =
         [
             new QuotesParsingStrategy(),
             new MultiLineCommentParsingStrategy(),
             new SingleLineCommentParsingStrategy(),
             new SemicolonParsingStrategy(),
             new ShouldCallParsingStrategy(),
+            new WhichParsingStrategy(),
             new AwaitParsingStrategy(),
             new AddNonEmptySymbolParsingStrategy()
         ];
     }
 
-    internal void Append(string symbols)
+    /// <summary>
+    /// Gets the identifiers preceding a Should or Which clause as extracted from lines of code passed to <see cref="Append"/>
+    /// </summary>
+    public string[] Identifiers => candidates.ToArray();
+
+    public void Append(string symbols)
     {
         using var symbolEnumerator = symbols.GetEnumerator();
 
-        while (symbolEnumerator.MoveNext() && parsingState != ParsingState.Done)
+        while (symbolEnumerator.MoveNext() && state != ParsingState.Completed)
         {
-            var hasParsingStrategyWaitingForEndContext = priorityOrderedParsingStrategies
-                .Exists(s => s.IsWaitingForContextEnd());
-
-            parsingState = ParsingState.InProgress;
-
-            foreach (var parsingStrategy in
-                     priorityOrderedParsingStrategies
-                         .SkipWhile(parsingStrategy =>
-                             hasParsingStrategyWaitingForEndContext
-                             && !parsingStrategy.IsWaitingForContextEnd()))
+            // The logic ensures that parsing does not continue with irrelevant strategies when a strategy is currently in the middle
+            // of a multi-symbol context (e.g., waiting for "_/" to match the beginning "/_"). In such cases, it skips over strategies
+            // that aren't waiting for the "end of context" while allowing the active (waiting) strategy to resume processing.
+            IEnumerable<IParsingStrategy> activeParsers = parsingStrategies;
+            if (parsingStrategies.Exists(s => s.IsWaitingForContextEnd()))
             {
-                parsingState = parsingStrategy.Parse(symbolEnumerator.Current, statement);
+                activeParsers = parsingStrategies.SkipWhile(parsingStrategy => !parsingStrategy.IsWaitingForContextEnd());
+            }
 
-                if (parsingState != ParsingState.InProgress)
+            state = ParsingState.InProgress;
+
+            foreach (IParsingStrategy parser in activeParsers)
+            {
+                state = parser.Parse(symbolEnumerator.Current, statement);
+                if (state == ParsingState.CandidateFound)
+                {
+                    candidates.Add(statement.ToString());
+                }
+
+                if (state != ParsingState.InProgress)
                 {
                     break;
                 }
@@ -57,11 +71,8 @@ internal class CallerStatementBuilder
             return;
         }
 
-        priorityOrderedParsingStrategies
-            .ForEach(strategy => strategy.NotifyEndOfLineReached());
+        parsingStrategies.ForEach(strategy => strategy.NotifyEndOfLineReached());
     }
 
-    internal bool IsDone() => parsingState == ParsingState.Done;
-
-    public override string ToString() => statement.ToString();
+    public bool IsDone() => state == ParsingState.Completed;
 }

Src/FluentAssertions/CallerIdentification/ParsingState.cs
@@ -2,7 +2,24 @@
 
 internal enum ParsingState
 {
+    /// <summary>
+    /// Is returned by a parser when the next one can take a look at the current symbol
+    /// </summary>
     InProgress,
+
+    /// <summary>
+    /// Is returned by a parser when it decides a symbol has been processed enough and no
+    /// other parsers need to look at the current symbol anymore.
+    /// </summary>
     GoToNextSymbol,
-    Done
+
+    /// <summary>
+    /// Is returned by a parser if it has found a candidate identifier.
+    /// </summary>
+    CandidateFound,
+
+    /// <summary>
+    /// Is returned by a parser to indicate that the parsing has been fully completed.
+    /// </summary>
+    Completed
 }

Src/FluentAssertions/CallerIdentification/SemicolonParsingStrategy.cs
@@ -9,7 +9,7 @@ internal class SemicolonParsingStrategy : IParsingStrategy
         if (symbol is ';')
         {
             statement.Clear();
-            return ParsingState.Done;
+            return ParsingState.Completed;
         }
 
         return ParsingState.InProgress;

Src/FluentAssertions/CallerIdentification/ShouldCallParsingStrategy.cs
@@ -2,36 +2,48 @@
 
 namespace FluentAssertions.CallerIdentification;
 
+/// <summary>
+/// Tries to determine if the statement ends with ".Should(" or ".Should.", and assumes
+/// the preceding identifier is the actual identifier.
+/// </summary>
 internal class ShouldCallParsingStrategy : IParsingStrategy
 {
-    private const string ShouldCall = ".Should";
+    private const string ExpectedPhrase = ".Should";
 
     public ParsingState Parse(char symbol, StringBuilder statement)
     {
-        if (statement.Length >= ShouldCall.Length + 1)
+        if (IsLongEnough(statement) && EndsWithExpectedPhrase(statement) && EndsWithInvocation(statement))
         {
-            var leftIndex = statement.Length - 2;
-            var rightIndex = ShouldCall.Length - 1;
-
-            for (var i = 0; i < ShouldCall.Length; i++)
-            {
-                if (statement[leftIndex - i] != ShouldCall[rightIndex - i])
-                {
-                    return ParsingState.InProgress;
-                }
-            }
-
-            if (statement[^1] is not ('(' or '.'))
-            {
-                return ParsingState.InProgress;
-            }
-
-            statement.Remove(statement.Length - (ShouldCall.Length + 1), ShouldCall.Length + 1);
-            return ParsingState.Done;
+            // Remove the ".Should." or ".Should(" part from the statement, so we keep the actual identifier
+            statement.Remove(statement.Length - (ExpectedPhrase.Length + 1), ExpectedPhrase.Length + 1);
+            return ParsingState.CandidateFound;
         }
 
         return ParsingState.InProgress;
     }
+
+    private static bool IsLongEnough(StringBuilder statement) => statement.Length >= ExpectedPhrase.Length + 1;
+
+    private static bool EndsWithExpectedPhrase(StringBuilder statement)
+    {
+        // Start from the index on the character just before the last ( or .
+        var rightIndexInStatement = statement.Length - 2;
+
+        var rightIndexInExpectedPhrase = ExpectedPhrase.Length - 1;
+
+        // Do a reverse comparison to see if the statement ends with ".Should"
+        for (var i = 0; i < ExpectedPhrase.Length; i++)
+        {
+            if (statement[rightIndexInStatement - i] != ExpectedPhrase[rightIndexInExpectedPhrase - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool EndsWithInvocation(StringBuilder statement) => statement[^1] is '(' or '.';
 
     public bool IsWaitingForContextEnd()
     {

Src/FluentAssertions/CallerIdentification/WhichParsingStrategy.cs
@@ -1,0 +1,54 @@
+using System.Text;
+
+namespace FluentAssertions.CallerIdentification;
+
+/// <summary>
+/// Tries to find the <c>.Which.</c> construct and assumes everything preceding it has become irrelevant
+/// for the chained assertion.
+/// </summary>
+internal class WhichParsingStrategy : IParsingStrategy
+{
+    private const string ExpectedPhrase = ".Which.";
+
+    public ParsingState Parse(char symbol, StringBuilder statement)
+    {
+        if (IsLongEnough(statement) && EndsWithExpectedPhrase(statement))
+        {
+            // Remove everything we collected up to know and assume everything following the
+            // .Which. property is a new assertion
+            statement.Clear();
+        }
+
+        return ParsingState.InProgress;
+    }
+
+    private static bool IsLongEnough(StringBuilder statement) => statement.Length >= ExpectedPhrase.Length;
+
+    private static bool EndsWithExpectedPhrase(StringBuilder statement)
+    {
+        // Start from the index of the last character
+        var rightIndexInStatement = statement.Length - 1;
+
+        var rightIndexInExpectedPhrase = ExpectedPhrase.Length - 1;
+
+        // Do a reverse comparison to see if the statement ends with ".Which."
+        for (var i = 0; i < ExpectedPhrase.Length; i++)
+        {
+            if (statement[rightIndexInStatement - i] != ExpectedPhrase[rightIndexInExpectedPhrase - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsWaitingForContextEnd()
+    {
+        return false;
+    }
+
+    public void NotifyEndOfLineReached()
+    {
+    }
+}

Src/FluentAssertions/CallerIdentifier.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
@@ -19,9 +20,28 @@ public static class CallerIdentifier
 {
     public static Action<string> Logger { get; set; } = _ => { };
 
+    /// <summary>
+    /// Gets the identifier that precedes the first Should call in the chain.
+    /// </summary>
     public static string DetermineCallerIdentity()
     {
-        string caller = null;
+        return DetermineCallerIdentities().FirstOrDefault();
+    }
+
+    /// <summary>
+    /// Gets all identifiers of all assertions in order of appearance.
+    /// </summary>
+    /// <example>
+    /// For example, given the following code
+    /// <code>collection.Should().ContainSingle()
+    ///     .Which.Parameters.Should().ContainSingle()
+    ///     .Which.Should().Be(3)
+    /// </code>
+    /// <see cref="DetermineCallerIdentity"/> will return <c>collection</c>, <c>Parameters</c> and an empty string.
+    /// </example>
+    public static string[] DetermineCallerIdentities()
+    {
+        string[] callers = [];
 
         try
         {
@@ -57,7 +77,7 @@ public static class CallerIdentifier
                     && !IsCustomAssertion(frame)
                     && !IsCurrentAssembly(frame))
                 {
-                    caller = ExtractVariableNameFrom(frame);
+                    callers = ExtractCallersFrom(frame).ToArray();
                     break;
                 }
             }
@@ -68,7 +88,7 @@ public static class CallerIdentifier
             Logger(e.ToString());
         }
 
-        return caller;
+        return callers;
     }
 
     private sealed class StackFrameReference : IDisposable
@@ -169,26 +189,23 @@ public static class CallerIdentifier
         return frame.GetMethod()?.DeclaringType?.Namespace is "System.Runtime.CompilerServices";
     }
 
-    private static string ExtractVariableNameFrom(StackFrame frame)
+    private static IEnumerable<string> ExtractCallersFrom(StackFrame frame)
     {
-        string caller = null;
-        string statement = GetSourceCodeStatementFrom(frame);
+        string[] identifiers = GetCallerIdentifiersFrom(frame);
 
-        if (!string.IsNullOrEmpty(statement))
+        foreach (string identifier in identifiers)
         {
-            Logger(statement);
+            Logger(identifier);
 
-            if (!IsBooleanLiteral(statement) && !IsNumeric(statement) && !IsStringLiteral(statement) &&
-                !StartsWithNewKeyword(statement))
+            if (!IsBooleanLiteral(identifier) && !IsNumeric(identifier) && !IsStringLiteral(identifier) &&
+                !StartsWithNewKeyword(identifier))
             {
-                caller = statement;
+                yield return identifier;
             }
         }
-
-        return caller;
     }
 
-    private static string GetSourceCodeStatementFrom(StackFrame frame)
+    private static string[] GetCallerIdentifiersFrom(StackFrame frame)
     {
         string fileName = frame.GetFileName();
         int expectedLineNumber = frame.GetFileLineNumber();
@@ -209,19 +226,18 @@ public static class CallerIdentifier
                 currentLine++;
             }
 
-            return currentLine == expectedLineNumber
-                && line != null
-                    ? GetSourceCodeStatementFrom(frame, reader, line)
+            return currentLine == expectedLineNumber && line != null
+                    ? GetCallerIdentifiersFrom(frame, reader, line)
                     : null;
         }
         catch
         {
             // We don't care. Just assume the symbol file is not available or unreadable
-            return null;
+            return [];
         }
     }
 
-    private static string GetSourceCodeStatementFrom(StackFrame frame, StreamReader reader, string line)
+    private static string[] GetCallerIdentifiersFrom(StackFrame frame, StreamReader reader, string line)
     {
         int column = frame.GetFileColumnNumber();
 
@@ -230,15 +246,15 @@ public static class CallerIdentifier
             line = line.Substring(Math.Min(column - 1, line.Length - 1));
         }
 
-        var sb = new CallerStatementBuilder();
+        var parser = new CallerStatementBuilder();
 
         do
         {
-            sb.Append(line);
+            parser.Append(line);
         }
-        while (!sb.IsDone() && (line = reader.ReadLine()) != null);
+        while (!parser.IsDone() && (line = reader.ReadLine()) != null);
 
-        return sb.ToString();
+        return parser.Identifiers;
     }
 
     private static bool StartsWithNewKeyword(string candidate)

commit 541af3b04949eeab4dc1404a0410189b95fc6fac

Handle missing caller identifier
Fixes #3031
Regression from #3000 where previously `getCallerIdentifier() + callerPostfix` would return an empty string.

Src/FluentAssertions/Execution/SubjectIdentificationBuilder.cs
@@ -83,7 +83,7 @@ internal class SubjectIdentificationBuilder
 
         if (scopeName is null)
         {
-            return callerIdentifier;
+            return callerIdentifier ?? "";
         }
         else if (callerIdentifier is null)
         {

Tests/FluentAssertions.Specs/Execution/AssertionChainSpecs.Chaining.cs
@@ -578,6 +578,20 @@ public partial class AssertionChainSpecs
             Assert.Contains("First \"assertion\"", failures);
         }
 
+        [Fact]
+        public void Returns_an_empty_identification_when_neither_scope_name_nor_caller_identifier_are_available()
+        {
+            // Arrange
+            var assertionChain = AssertionChain.GetOrCreate();
+            assertionChain.OverrideCallerIdentifier(() => null);
+
+            // Act
+            string identification = assertionChain.CallerIdentifier;
+
+            // Assert
+            identification.Should().BeEmpty();
+        }
+
         // [Fact]
         // public void Get_info_about_line_breaks_from_parent_scope_after_continuing_chained_assertion()
         // {