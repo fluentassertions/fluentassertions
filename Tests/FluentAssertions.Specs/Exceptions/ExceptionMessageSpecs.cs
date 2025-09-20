c083348deff5dbbcbbc04d8cdf4bfa2e6e03bd71
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2025-09-20 19:01:44 +0200

    SAVEPOINT

---
+++ Tests/FluentAssertions.Specs/Exceptions/ExceptionMessageSpecs.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace FluentAssertions.Specs.Exceptions;
+
+public class ExceptionMessageSpecs
+{
+    [Fact]
+    public void Can_assert_the_exception_message_does_not_include_a_specific_string()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+    }
+}
